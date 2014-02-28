9f36542ae9ac014e06ec35c0fb1b6925d5a83445
Dan Blanchard <dblanchard@ets.org>
2014-02-28 14:51:54 -0500

    Future import on main_index.py

--- conda_build/main_index.py
+++ conda_build/main_index.py
@@ -1,4 +1,5 @@
-from __future__ import print_function, division, absolute_import
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
 
 import os
 from os.path import abspath
