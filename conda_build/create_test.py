3eb466e7a7d2968f6f8dac3eba19344d4e20afb8
Dan Blanchard <dblanchard@ets.org>
2014-03-11 13:05:51 -0400

    Skip printing version in create_pl_files when name contains spaces.

--- conda_build/create_test.py
+++ conda_build/create_test.py
@@ -86,9 +86,11 @@ def create_pl_files(dir_path, m):
         for name in m.get_value('test/imports'):
             print(r'print("import: %s\n");' % name, file=fo)
             print('use %s;\n' % name, file=fo)
-            print(("if (defined {0}->VERSION) {{\n\t" +
-                   "print('\tusing version ' . {0}->VERSION . '\n');\n" +
-                   "\n}}").format(name), file=fo)
+            # Don't try to print version for complex imports
+            if ' ' not in name:
+                print(("if (defined {0}->VERSION) {{\n\t" +
+                       "print('\tusing version ' . {0}->VERSION . '\n');\n" +
+                       "\n}}").format(name), file=fo)
             has_tests = True
 
         try:
