71649a17233b6471b0ff586b8611e8104886c821
Trent Nelson <trent@trent.me>
2014-05-20 23:42:06 -0400

    Checkpoint commit of tonight's progress.
    Everything is coming together nicely.  Dynamic runtime path inference is
    working, next and final step is to implement the actual patchelf calls
    that set the RPATH (on Linux... OS X and Windows will be later).

--- conda_build/dll.py
+++ conda_build/dll.py
@@ -1,6 +1,8 @@
 #===============================================================================
 # Imports
 #===============================================================================
+import os
+import re
 import sys
 
 from abc import (
@@ -8,11 +10,17 @@ from abc import (
     abstractmethod,
 )
 
+from os import (
+    readlink,
+)
+
 from os.path import (
     join,
+    islink,
     abspath,
-    normpath,
+    dirname,
     basename,
+    normpath,
 )
 
 from subprocess import (
@@ -78,6 +86,277 @@ def version_combinations(s):
         return None
 
     return [ '.'.join(ints[:x]) for x in reversed(range(1, len(ints)+1)) ]
+
+def invert_defaultdict_by_value_len(d):
+    i = {}
+    for (k, v) in d.items():
+        i.setdefault(len(v), []).append(k)
+    return i
+
+def package_name_providing_link_target(libname):
+    # xxx todo: perhaps dispatch this inquiry to a server-side request that
+    # can better answer the question of "are there *any* conda packages that
+    # provide this dll to link against?".  For now, just return None.
+    return None
+
+#===============================================================================
+# Path-related Helpers
+#===============================================================================
+def get_base_dir(path):
+    p = path
+    pc = p.count('/')
+    assert p and p[0] == '/' and pc >= 1
+    if p == '/' or pc == 1 or (pc == 2 and p[-1] == '/'):
+        return '/'
+
+    assert pc >= 2
+    return dirname(p[:-1] if p[-1] == '/' else p) + '/'
+
+def reduce_path(p):
+    assert p and p[0] == '/'
+    r = list()
+    end = p.rfind('/')
+    while end != -1:
+        r.append(p[:end+1])
+        end = p.rfind('/', 0, end)
+    return r
+
+def join_path(*args):
+    return abspath(normpath(join(*args)))
+
+def format_path(path, is_dir=None):
+    """
+    >>> format_path('src', True)
+    '/src/'
+
+    >>> format_path('src', False)
+    '/src'
+
+    >>> format_path('src/foo', True)
+    '/src/foo/'
+
+    >>> format_path('///src///foo///mexico.txt//', False)
+    '/src/foo/mexico.txt'
+
+    >>> format_path('///src///foo///mexico.txt//')
+    '/src/foo/mexico.txt/'
+
+    >>> format_path('///src///foo///mexico.txt')
+    '/src/foo/mexico.txt'
+
+    >>> format_path(r'\\the\\quick\\brown\\fox.txt', False)
+    '/\\\\the\\\\quick\\\\brown\\\\fox.txt'
+
+    >>> format_path('/')
+    '/'
+
+    >>> format_path('/', True)
+    '/'
+
+    >>> format_path('/', False)
+    Traceback (most recent call last):
+        ...
+    AssertionError
+
+    >>> format_path('/a')
+    '/a'
+
+    >>> format_path('/ab')
+    '/ab'
+
+    >>> format_path(None)
+    Traceback (most recent call last):
+        ...
+    AssertionError
+
+    >>> format_path('//')
+    Traceback (most recent call last):
+        ...
+    AssertionError
+
+    >>> format_path('/', True)
+    '/'
+
+    # On Unix, '\' is a legitimate file name.  Trying to wrangle the right
+    # escapes when testing '/' and '\' combinations is an absolute 'mare;
+    # so we use ord() instead to compare numerical values of characters.
+    >>> _w = lambda p: [ ord(c) for c in p ]
+    >>> b = chr(92) # forward slash
+    >>> f = chr(47) # backslash
+    >>> foo = [102, 111, 111] # ord repr for 'foo'
+    >>> b2 = b*2
+    >>> _w(format_path('/'+b))
+    [47, 92]
+
+    >>> _w(format_path('/'+b2))
+    [47, 92, 92]
+
+    >>> _w(format_path('/'+b2, is_dir=False))
+    [47, 92, 92]
+
+    >>> _w(format_path('/'+b2, is_dir=True))
+    [47, 92, 92, 47]
+
+    >>> _w(format_path(b2*2))
+    [47, 92, 92, 92, 92]
+
+    >>> _w(format_path(b2*2, is_dir=True))
+    [47, 92, 92, 92, 92, 47]
+
+    >>> _w(format_path('/foo/'+b))
+    [47, 102, 111, 111, 47, 92]
+
+    >>> _w(format_path('/foo/'+b, is_dir=False))
+    [47, 102, 111, 111, 47, 92]
+
+    >>> _w(format_path('/foo/'+b, is_dir=True))
+    [47, 102, 111, 111, 47, 92, 47]
+
+    """
+    assert (
+        path and
+        path not in ('//', '///') and
+        is_dir in (True, False, None)
+    )
+
+    if path == '/':
+        assert is_dir in (True, None)
+        return '/'
+
+    p = path
+    while True:
+        if re.search('//', p):
+            p = p.replace('//', '/')
+        else:
+            break
+
+    if p == '/':
+        assert is_dir in (True, None)
+        return '/'
+
+    if p[0] != '/':
+        p = '/' + p
+
+    if is_dir is True:
+        if p[-1] != '/':
+            p += '/'
+    elif is_dir is False:
+        if p[-1] == '/':
+            p = p[:-1]
+
+    return p
+
+def format_dir(path):
+    return format_path(path, is_dir=True)
+
+def format_file(path):
+    return format_path(path, is_dir=False)
+
+def assert_no_file_dir_clash(paths):
+    """
+    >>> assert_no_file_dir_clash('lskdjf')
+    Traceback (most recent call last):
+        ...
+    AssertionError
+
+    >>> assert_no_file_dir_clash(False)
+    Traceback (most recent call last):
+        ...
+    AssertionError
+
+    >>> assert_no_file_dir_clash(['/src/', '/src/'])
+    Traceback (most recent call last):
+        ...
+    AssertionError
+
+    >>> assert_no_file_dir_clash(['/src', '/src/'])
+    Traceback (most recent call last):
+        ...
+    AssertionError
+
+    >>> assert_no_file_dir_clash(['/sr', '/src/', '/srcb/'])
+    >>>
+
+    """
+    assert paths and hasattr(paths, '__iter__')
+    seen = set()
+    for p in paths:
+        assert not p in seen
+        seen.add(p)
+
+    assert all(
+        (p[:-1] if p[-1] == '/' else p + '/') not in seen
+            for p in paths
+    )
+
+
+def get_root_path(paths):
+    """
+    Given a list of paths (directories or files), return the root directory or
+    an empty string if no root can be found.
+
+    >>> get_root_path(['/src/', '/src/trunk/', '/src/trunk/test.txt'])
+    '/src/'
+    >>> get_root_path(['/src/', '/src/trk/', '/src/trk/test.txt', '/src/a'])
+    '/src/'
+    >>> get_root_path(['/', '/laksdjf', '/lkj'])
+    '/'
+    >>> get_root_path(['/'])
+    '/'
+    >>> get_root_path(['/a'])
+    '/'
+    >>>
+    >>> get_root_path(['/src/trunk/foo.txt', '/src/tas/2009.01.00/foo.txt'])
+    '/src/'
+    >>> get_root_path(['/src/branches/foo/'])
+    '/src/branches/foo/'
+
+    >>> get_root_path(['',])
+    Traceback (most recent call last):
+        ...
+    AssertionError
+
+    >>> get_root_path(['lskdjf',])
+    Traceback (most recent call last):
+        ...
+    AssertionError
+
+    >>> get_root_path(['src/trunk/',])
+    Traceback (most recent call last):
+        ...
+    AssertionError
+
+    >>> get_root_path(['/src/trunk/', '/src/trunk'])
+    Traceback (most recent call last):
+        ...
+    AssertionError
+    """
+    assert (
+        hasattr(paths, '__iter__')   and
+        #len(paths) >= 1              and
+        all(d and d[0] == '/' for d in paths)
+    )
+
+    def _parts(p):
+        parts = p.split('/')
+        return parts if p[-1] == '/' else parts[:-1]
+
+    paths = [ format_path(p) for p in paths ]
+    assert_no_file_dir_clash(paths)
+
+    common = _parts(paths[0])
+
+    for j in range(1, len(paths)):
+        parts =  _parts(paths[j])
+        for i in range(len(common)):
+            if i == len(parts) or common[i] != parts[i]:
+                del common[i:]
+                break
+    if not common or (len(common) == 1 and common[0] == ''):
+        return '/'
+
+    return format_dir('/'.join(common))
+
 
 #===============================================================================
 # Library-related Helpers
@@ -164,6 +443,31 @@ def parse_ldd_output(output):
         results.append((depname, target))
 
     return results
+
+def relative_path(library_path, target_dir):
+    """
+    >>> dll = 'lib/python2.7/site-packages/rpy2/rinterface/_rinterface.so'
+    >>> relative_path(dll, 'lib')
+    '../../../..'
+    >>> relative_path(dll, 'lib64/R/lib')
+    '../../../../../lib64/R/lib'
+    >>> relative_path(dll, 'lib/python2.7/site-packages/rpy2/rinterface')
+    ''
+    """
+    if dirname(library_path) == target_dir:
+        return ''
+
+    paths = [ format_file(library_path), format_dir(target_dir) ]
+    root = get_root_path(paths)
+
+    slashes = library_path.count('/')
+    if root != '/':
+        slashes -= 1
+        suffix = ''
+    else:
+        suffix = '/' + target_dir
+
+    return normpath(slashes * '../') + suffix
 
 def get_library_dependencies(dll):
     if is_linux:
@@ -271,11 +575,30 @@ class LibraryDependencies(SlotObject):
             elif target.startswith(prefix):
                 inside.add(depname)
             else:
-                outside.add(depname)
+                outside.add(target)
 
         self.inside = inside
         self.outside = outside
         self.missing = missing
+
+class LinkError_RecipeCorrectButBuildScriptBroken(SlotObject, BaseException):
+    __slots__ = (
+        'dependent_library_name',
+        'expected_link_target',
+        'actual_link_target',
+    )
+    def __init__(self, *args):
+        SlotObject.__init__(self, *args)
+        self.message = repr(self)
+
+class LinkError_MissingPackageDependencyInRecipe(SlotObject, BaseException):
+    __slots__ = (
+        'dependent_library_name',
+        'missing_package_dependency',
+    )
+    def __init__(self, *args):
+        SlotObject.__init__(self, *args)
+        self.message = repr(self)
 
 class DynamicLibrary(LibraryDependencies):
     __metaclass__ = ABCMeta
@@ -283,18 +606,75 @@ class DynamicLibrary(LibraryDependencies):
         'path',
         'prefix',
         'relative',
+        'build_root',
+        'runtime_paths',
+        'relative_runtime_paths',
     )
 
-    def __init__(self, path, prefix=None):
-        if not prefix:
-            prefix = build_prefix
-
+    def __init__(self, path, build_root):
         self.path = path
-        self.prefix = prefix
-        self.relative = path.replace(prefix, '')
+        self.prefix = build_root.prefix
+        self.relative = path.replace(self.prefix, '')[1:]
+        self.build_root = build_root
 
         deps = get_library_dependencies(path)
-        LibraryDependencies.__init__(self, deps, prefix)
+        LibraryDependencies.__init__(self, deps, self.prefix)
+
+        self._check_outside_targets()
+        self._resolve_inside_and_missing_targets()
+        self._resolve_relative_runtime_paths()
+
+    def _check_outside_targets(self):
+        # For targets outside the build root, we want to make sure the target
+        # isn't something that should actually be linking to within the build
+        # prefix.  For now, we just check for targets that also appear in the
+        # build prefix... which helps if you've set the meta.yaml dependencies
+        # correctly but balked your build.
+        #
+        # We can improve this down the track such that conda has awareness of
+        # *all* deps (i.e. every .so of every official package), such that we
+        # could detect the (arguably more common) situation where a recipe is
+        # missing the required dependencies.
+        #
+        # (Further down the track, I'd like to see this sort of awareness
+        # harnessed to build tools that help automate the writing of recipes;
+        # the less domain knowledge you need to leverage the power of conda
+        # build, the more widespread the adoption.)
+
+        build_root = self.build_root
+        for path in self.outside:
+            name = basename(path)
+            if name in build_root:
+                cls = LinkError_RecipeCorrectButBuildScriptBroken
+                expected = build_root[name]
+                actual = path
+                raise cls(name, expected, actual)
+            else:
+                package = package_name_providing_link_target(name)
+                if not package:
+                    continue
+                cls = LinkError_MissingPackageDependencyInRecipe
+                raise cls(name, package)
+
+    def _resolve_inside_and_missing_targets(self):
+
+        build_root = self.build_root
+        rpaths = set()
+        for attr in ('inside', 'missing'):
+            libs = getattr(self, attr)
+            for name in libs:
+                rpaths.add(build_root[name])
+                continue
+
+        self.runtime_paths = rpaths
+
+    def _resolve_relative_runtime_paths(self):
+        assert self.runtime_paths, repr(self)
+        self.relative_runtime_paths = [
+            relative_path(self.relative, target)
+                for target in self.runtime_paths
+        ]
+
 
     @classmethod
     def create(cls, *args, **kwds):
@@ -313,7 +693,7 @@ class DynamicLibrary(LibraryDependencies):
 
     @property
     @memoize
-    def runtime_path_dependencies(self):
+    def rpath(self):
         pass
 
 
@@ -339,6 +719,7 @@ class Win32DynamicLibrary(DynamicLibrary):
 class BuildRoot(SlotObject):
     __slots__ = (
         'prefix',
+        'relative_start_index',
 
         'old_files',
         'all_files',
@@ -350,10 +731,14 @@ class BuildRoot(SlotObject):
 
         'old_dll_paths',
         'all_dll_paths',
+        'all_symlink_dll_paths',
         'new_dll_paths',
 
         'old_dlls',
         'all_dlls',
+        'all_dlls_by_len',
+        'all_symlink_dlls',
+        'all_symlink_dlls_by_len',
         'new_dlls',
     )
 
@@ -363,6 +748,7 @@ class BuildRoot(SlotObject):
         if not prefix:
             prefix = build_prefix
         self.prefix = prefix
+        self.relative_start_ix = len(prefix)+1
 
         if not old_files:
             # Ugh, this should be abstracted into a single interface that we
@@ -385,9 +771,9 @@ class BuildRoot(SlotObject):
         # (which is the thing that should be calling us).
         from conda_build.post import is_obj
 
-        self.old_paths = (join(prefix, f) for f in self.old_files)
-        self.all_paths = (join(prefix, f) for f in self.all_files)
-        self.new_paths = (join(prefix, f) for f in self.new_files)
+        self.old_paths = [ join(prefix, f) for f in self.old_files ]
+        self.all_paths = [ join(prefix, f) for f in self.all_files ]
+        self.new_paths = [ join(prefix, f) for f in self.new_files ]
 
         self.old_dll_paths = set(p for p in self.old_paths if is_obj(p))
         self.all_dll_paths = set(
@@ -402,15 +788,60 @@ class BuildRoot(SlotObject):
             name = basename(path)
             self.all_dlls[name].append(path)
 
+        self.all_symlink_dll_paths = [
+            p for p in self.all_paths
+                if islink(p) and basename(readlink(p)) in self.all_dlls
+        ]
+
+        self.all_symlink_dlls = defaultdict(list)
+        for path in self.all_symlink_dll_paths:
+            name = basename(path)
+            self.all_symlink_dlls[name].append(path)
+
+        # Invert both dicts such that the keys become the length of the lists;
+        # in a perfect world, there would only be one key, [1], which means
+        # all the target filenames were unique within the entire build root.
+        #
+        # R has one with two:
+        #
+        #    In [75]: br.all_dlls_by_len.keys()
+        #    Out[75]: [1, 2]
+        #
+        #    In [76]: br.all_dlls_by_len[2]
+        #    Out[76]: [u'Rscript']
+        #
+        #    In [77]: br.all_dlls['Rscript']
+        #    Out[77]:
+        #    [u'/home/r/miniconda/envs/_build/bin/Rscript',
+        #     u'/home/r/miniconda/envs/_build/lib64/R/bin/Rscript']
+        #
+        # In the case above, we can ignore this one, as nothing links to
+        # Rscript directly (technically, it's an executable, but is_elf()
+        # can't distinguish between exe and .so).  If libR.so was showing
+        # two hits, that's a much bigger problem (we'll trap that via an
+        # assert in our __getitem__()).
+        self.all_dlls_by_len = invert_defaultdict_by_value_len(self.all_dlls)
+        self.all_symlink_dlls_by_len = (
+            invert_defaultdict_by_value_len(self.all_symlink_dlls)
+        )
+
         self.new_dlls = [
-            DynamicLibrary.create(p, prefix)
+            DynamicLibrary.create(p, build_root=self)
                 for p in sorted(self.new_dll_paths)
         ]
 
-        # xxx todo: enumerate over each new dll and looking for dodgy 'outside'
-        # links that indicate missing deps in the yaml, or 'missing' libs that
-        # we need to find equivalents for in all_dlls (and thus set an rpath
-        # with multiple paths).
+    def __getitem__(self, dll_name):
+        targets = self.all_dlls.get(dll_name, self.all_symlink_dlls[dll_name])
+        assert len(targets) == 1, (dll_name, targets)
+        target = targets[0]
+        relative_target = target[self.relative_start_ix:]
+        return dirname(relative_target)
+
+    def __contains__(self, dll_name):
+        return (
+            dll_name in self.all_dlls or
+            dll_name in self.all_symlink_dlls
+        )
 
 
     def run_post(self):
