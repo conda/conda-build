59cf5477b602a1743afd1bb9d3a1e05d25b17354
Ilan Schnell <ilanschnell@gmail.com>
2014-06-09 14:37:29 -0500

    add Python 3.4 to Trove classifiers

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
     ],
     description = "tools for building conda packages",
     long_description = open('README.rst').read(),
