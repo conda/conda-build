bc726adcc827a7e92fa61f5ee00090fec61444e5
Aaron Meurer <asmeurer@gmail.com>
2014-06-26 16:24:35 -0500

    Some more skeleton work on bdist_conda
    - Subclass from the install command. We want to install into the build prefix
    and then wrap it up into a conda package, so (hopefully) we can just let
    install do its thing and then do a conda build.
    
    - We don't actually generate the metadata yet.
    
    - No idea if this will actually work. We'll have to find out.

--- bdist_conda.py
+++ bdist_conda.py
@@ -5,16 +5,37 @@ bdist_conda
 from __future__ import (print_function, division, unicode_literals,
     absolute_import)
 
-from distutils.core import Command
+from collections import defaultdict
 
-class bdist_conda(Command):
+from distutils.command.install import install
+from distutils.errors import DistutilsOptionError
+
+from conda.lock import Locked
+from conda_build.metadata import MetaData
+from conda_build import config, build
+
+# TODO: Add support for all the options that conda build has
+
+class bdist_conda(install):
     description = "create a conda package"
 
     def initialize_options(self):
-        pass
+        super(bdist_conda, self).initialize_options()
 
     def finalize_options(self):
-        pass
+        super(bdist_conda, self).finalize_options()
+        if self.prefix:
+            raise DistutilsOptionError("--prefix is not allowed")
+        self.prefix = config.build_prefix
 
     def run(self):
-        pass
+        with Locked(config.croot):
+            super(bdist_conda, self).run()
+            d = defaultdict(dict)
+            # Insert metadata here
+            d = dict(d)
+            m = MetaData.fromdict(d)
+            # Shouldn't fail, but do you really trust the code above?
+            m.check_fields()
+            build.build(m)
+            build.test(m)
