2e3edd6c314077222f921010fb28db5876e5c40d
Aaron Meurer <asmeurer@gmail.com>
2014-07-29 15:18:41 -0500

    Fix a Python 3 issue

--- conda_build/environ.py
+++ conda_build/environ.py
@@ -39,6 +39,7 @@ def get_git_build_info(src_dir):
     process = subprocess.Popen(["git", "describe", "--tags", "--long", "HEAD"],
                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output = process.communicate()[0].strip()
+    output = output.decode('utf-8')
     parts = output.rsplit('-', 2)
     parts_length = len(parts)
     if parts_length == 3:
