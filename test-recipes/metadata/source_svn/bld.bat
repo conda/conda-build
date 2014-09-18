668100cd08427c5bc45eebb37e6fefd0f3963f14
Aaron Meurer <asmeurer@gmail.com>
2014-09-18 16:30:43 -0500

    Fix source_svn test recipe on Windows

--- test-recipes/metadata/source_svn/bld.bat
+++ test-recipes/metadata/source_svn/bld.bat
@@ -2,5 +2,7 @@ if not exist trunk exit 1
 cd trunk
 svn info
 if errorlevel 1 exit 1
-for /f "delims=" %%i in ('svn info | grep "Revision"') do set svnrev=%%i
-if not svnrev=="Revision: 1157" exit 1
+for /f "delims=" %%i in ('svn info ^| grep "Revision"') do set svnrev=%%i
+if errorlevel 1 exit 1
+echo %svnrev%
+if not "%svnrev%"=="Revision: 1157" exit 1
