67d6831e51900230f048065143ddb8b7bd362051
Aaron Meurer <asmeurer@gmail.com>
2014-09-18 16:34:33 -0500

    Add bld.bat for source_git test recipe

---
+++ test-recipes/metadata/source_git/bld.bat
@@ -0,0 +1,10 @@
+if not exist .git exit 1
+git describe
+if errorlevel 1 exit 1
+for /f "delims=" %%i in ('git describe') do set gitdesc=%%i
+if errorlevel 1 exit 1
+echo "%gitdesc%"
+if not "%gitdesc%"=="1.8.1" exit 1
+set PYTHONPATH=.
+python -c "import conda_build; assert conda_build.__version__ == '1.8.1'"
+if errorlevel 1 exit 1
