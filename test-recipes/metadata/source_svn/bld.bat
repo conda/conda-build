701d6e8993d4f7c36717748efaacfc9ca617f68e
Aaron Meurer <asmeurer@gmail.com>
2014-09-18 15:00:08 -0500

    Add bld.bat to source_svn test recipe

---
+++ test-recipes/metadata/source_svn/bld.bat
@@ -0,0 +1,7 @@
+if not exist trunk exit 1
+cd trunk
+svn info
+if errorlevel 1 exit 1
+for /f "delims=" %%i in ('svn info | grep "Revision"') do set svnrev=%%i
+if errorlevel 1 exit 1
+if not svnrev=="Revision: 1157" exit 1
