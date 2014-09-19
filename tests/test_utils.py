8260d7556e230f8b161b066db602795a1d7c08fe
Ilan Schnell <ilanschnell@gmail.com>
2014-09-19 12:52:42 -0500

    add more tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -19,7 +19,7 @@ class TestUtils(unittest.TestCase):
             ]:
             self.assertEqual(utils.relative(f), r)
 
-    def test_relative(self):
+    def test_relative_lib(self):
         for d, f, r in [
             ('lib', 'bin/python', '../lib'),
             ('lib', 'lib/libhdf5.so', '.'),
@@ -28,15 +28,28 @@ class TestUtils(unittest.TestCase):
             ('lib', 'lib/python2.6/site-packages/pyodbc.so', '../..'),
             ('lib', 'lib/python2.6/site-packages/bsdiff3/core.so', '../../..'),
             ('lib', 'xyz', './lib'),
+            ('lib', 'bin/somedir/cmd', '../../lib'),
+            ('lib', 'bin/somedir/somedir2/cmd', '../../../lib'),
+            ]:
+            self.assertEqual(utils.relative(f, d), r)
+
+    def test_relative_prefix(self):
+        for d, f, r in [
             ('.', 'xyz', '.'),
             ('.', 'bin/xyz', '..'),
             ('.', 'a/b/xyz', '../..'),
-            ('lib', 'bin/somedir/cmd', '../../lib'),
-            ('lib', 'bin/somedir/somedir2/cmd', '../../../lib'),
+            ('.', 'a/b/c/xyz', '../../..'),
+            ('.', 'a/b/c/d/xyz', '../../../..'),
+            ]:
+            self.assertEqual(utils.relative(f, d), r)
+
+    def test_relative_subdirs(self):
+        for d, f, r in [
             ('lib/sub', 'bin/somedir/cmd', '../../lib/sub'),
             ('lib/sub', 'bin/python', '../lib/sub'),
             ('lib/sub', 'lib/sub/libhdf5.so', '.'),
             ('a/b/c', 'a/b/c/libhdf5.so', '.'),
             ('a/b/c/d', 'a/b/x/y/libhdf5.so', '../../c/d'),
+            ('a/b/c/d', 'a/b/c/y/libhdf5.so', '../d'),
             ]:
             self.assertEqual(utils.relative(f, d), r)
