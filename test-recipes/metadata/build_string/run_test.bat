d5afdce7ca16674d7840c2a1eaff600ad35d3815
Aaron Meurer <asmeurer@gmail.com>
2014-09-19 16:33:34 -0500

    Use correct build string in metadata files

--- test-recipes/metadata/build_string/run_test.bat
+++ test-recipes/metadata/build_string/run_test.bat
@@ -3,10 +3,10 @@ if errorlevel 1 exit 1
 for /f "delims=" %%i in ('conda list -p "%PREFIX%" --canonical') do set condalist=%%i
 if errorlevel 1 exit 1
 echo "%condalist%"
-if not "%condalist%"=="conda-build-test-build-string-1.0-1" exit 1
-cat "%PREFIX%\conda-meta\conda-build-test-build-string-1.0-1.json"
+if not "%condalist%"=="conda-build-test-build-string-1.0-abc" exit 1
+cat "%PREFIX%\conda-meta\conda-build-test-build-string-1.0-abc.json"
 if errorlevel 1 exit 1
-cat "%PREFIX%\conda-meta\conda-build-test-build-string-1.0-1.json" | grep '"build_number": 0'
+cat "%PREFIX%\conda-meta\conda-build-test-build-string-1.0-abc.json" | grep '"build_number": 0'
 if errorlevel 1 exit 1
-cat "%PREFIX%\conda-meta\conda-build-test-build-string-1.0-1.json" | grep '"build": "abc"'
+cat "%PREFIX%\conda-meta\conda-build-test-build-string-1.0-abc.json" | grep '"build": "abc"'
 if errorlevel 1 exit 1
