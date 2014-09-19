11cb98213ef1e995a993d4025fcef62a4e0e47d6
Darren Dale <dsdale24@gmail.com>
2014-09-19 17:11:38 -0400

    updates tests

--- test-recipes/metadata/has_prefix_files/build.sh
+++ test-recipes/metadata/has_prefix_files/build.sh
@@ -1,3 +1,3 @@
 echo $PREFIX > $PREFIX/automatic-prefix
 echo /opt/anaconda1anaconda2anaconda3 > $PREFIX/has-prefix
-python $RECIPE_DIR/test-recipes/metadata/has_prefix_files/write_binary_has_prefix.py
+python $RECIPE_DIR/write_binary_has_prefix.py
