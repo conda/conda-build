dc282e548733632028335a0ed31fa6d43c2cbac2
Aaron Meurer <asmeurer@gmail.com>
2014-09-19 16:37:47 -0500

    Add missing "if errorlevel 1 exit 1" lines

--- test-recipes/metadata/entry_points/run_test.bat
+++ test-recipes/metadata/entry_points/run_test.bat
@@ -3,7 +3,9 @@ rem Won't be found because there are no shebang lines on Windows
 rem python test-script-setup.py
 rem if errorlevel 1 exit 1
 rem python test-script-setup.py | grep "Test script setup\.py"
+rem if errorlevel 1 exit 1
 
 test-script-manual
 if errorlevel 1 exit 1
 test-script-manual | grep "Manual entry point"
+if errorlevel 1 exit 1
