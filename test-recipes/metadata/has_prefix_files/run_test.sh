437ac659743728ab06845ea53438160b241701f1
Darren Dale <dsdale24@gmail.com>
2014-09-19 17:25:27 -0400

    updates tests

--- test-recipes/metadata/has_prefix_files/run_test.sh
+++ test-recipes/metadata/has_prefix_files/run_test.sh
@@ -4,5 +4,3 @@ cat automatic-prefix | grep $PREFIX
 cat has-prefix
 cat has-prefix | grep $PREFIX
 cat has-prefix | grep -v /opt/anaconda1anaconda2anaconda3
-cat binary-has-prefix
-cat binary-has-prefix | grep $PREFIX
