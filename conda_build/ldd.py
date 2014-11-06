0158579b9a6c729e7af9a543caeef25018e07834
Aaron Meurer <asmeurer@gmail.com>
2014-11-06 17:17:07 -0600

    Add first pass at a get_package_linkages function

--- conda_build/ldd.py
+++ conda_build/ldd.py
@@ -2,8 +2,15 @@ from __future__ import absolute_import, division, print_function
 
 import re
 import subprocess
+import json
+from os.path import join
+
+from conda.install import rm_rf
 
 from conda_build import post
+from conda_build.config import config
+from conda_build.build import create_env
+
 
 LDD_RE = re.compile(r'\s*(.*?)\s*=>\s*(.*?)\s*\(.*\)')
 LDD_NOT_FOUND_RE = re.compile(r'\s*(.*?)\s*=>\s*not found')
@@ -28,5 +35,24 @@ def ldd(path):
         if 'ld-linux' in line:
             continue
         raise RuntimeError("Unexpected output from ldd: %s" % line)
+
+    return res
+
+def get_package_linkages(pkg):
+    rm_rf(config.test_prefix)
+    specs = ['%s %s %s' % (pkg.rsplit('.tar.bz2', 1)[0].rsplit('-', 2))]
+
+    create_env(config.test_prefix, specs)
+
+    res = {}
+
+    with open(join(config.test_prefix, 'conda-meta', '-'.join(specs[0]) +
+        '.json')) as f:
+        data = json.load(f)
+
+    files = data['files']
+    for f in files:
+        if post.is_obj(f):
+            res[f] = ldd(f)
 
     return res
