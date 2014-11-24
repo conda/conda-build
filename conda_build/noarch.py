6e012940fab6823b8eeb951e5f71bcb49bda73d0
Ilan Schnell <ilanschnell@gmail.com>
2014-11-24 14:25:51 -0600

    add creation of Windows batch files

--- conda_build/noarch.py
+++ conda_build/noarch.py
@@ -10,6 +10,12 @@ SP_DIR=$($PREFIX/bin/python -c "from distutils.sysconfig import get_python_lib; 
 #echo "SP_DIR='$SP_DIR'"
 '''
 
+BAT_HEAD = '''\
+for /f %%i in ('%PREFIX%/python.exe -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())"') do set SP_DIR=%%i
+if errorlevel 1 exit 1
+#echo "SP_DIR='$SP_DIR'"
+'''
+
 def handle_file(f):
     path = join(config.build_prefix, f)
     if f.endswith(('.egg-info', '.pyc')):
@@ -31,13 +37,24 @@ def handle_file(f):
 def transform(m, files):
     f1 = open(join(config.build_prefix,
                    'bin/.%s-pre-link.sh' % m.name()), 'w')
+    f1.write(BASH_HEAD)
     f1.write('''
 cp $SOURCE_DIR/bin/.%s-pre-unlink.sh $PREFIX/bin
 ''' % m.name())
-    f1.write(BASH_HEAD)
     f2 = open(join(config.build_prefix,
                    'bin/.%s-pre-unlink.sh' % m.name()), 'w')
     f2.write(BASH_HEAD)
+
+    f3 = open(join(config.build_prefix,
+                   'Scripts', '.%s-pre-link.bat' % m.name()), 'w')
+    f3.write(BAT_HEAD)
+    f3.write('''
+copy %%SOURCE_DIR%%/Scripts/.%s-pre-unlink.bat %%PREFIX%%/Scripts/
+''' % m.name())
+    f4 = open(join(config.build_prefix,
+                   'Scripts', '.%s-pre-unlink.bat' % m.name()), 'w')
+    f4.write(BAT_HEAD)
+
     dirs = set()
     for f in files:
         g = handle_file(f)
@@ -53,9 +70,18 @@ ln $SOURCE_DIR/site-packages/%s $SP_DIR/%s
 ''' % (dirname(g), g, g, g))
             f2.write('rm -f $SP_DIR/%s*\n' % g)
 
-    f1.close()
+            f3.write('''
+md %%SP_DIR%%/%s
+del %%SP_DIR%%/%s
+fsutil hardlink create %%SP_DIR%%/%s %%SOURCE_DIR%%/site-packages/%s
+''' % (dirname(g), g, g, g))
+            f4.write('del %%SP_DIR%%/%s*\n' % g)
 
     for d in sorted(dirs, key=len, reverse=True):
         f2.write('rmdir $SP_DIR/%s\n' % d)
+        f4.write('rd /S /Q %%SP_DIR%%/%s\n' % d)
 
+    f1.close()
     f2.close()
+    f3.close()
+    f4.close()
