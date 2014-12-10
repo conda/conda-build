c3e60c7d5647044a13535a15c01361bd594c1c8f
Aaron Meurer <asmeurer@gmail.com>
2014-12-10 16:57:18 -0600

    Check the md5 of the installer and install it

--- tests/install_miniconda.py
+++ tests/install_miniconda.py
@@ -5,7 +5,7 @@ import subprocess
 
 tempdir = os.environ.get('TEMP', '/tmp')
 
-def download_file(url):
+def download_file(url, md5):
     urlparts = requests.packages.urllib3.util.url.parse_url(url)
     # local_filename = urlparts.path.split('/')[-1]
     local_filename = "Miniconda.exe"
@@ -15,8 +15,15 @@ def download_file(url):
 
     dir_path = os.path.join(tempdir, 'download_cache')
     file_path = os.path.join(dir_path, local_filename)
-    print("Downloading %s to %s" % (local_filename, file_path))
+    if os.path.exists(file_path):
+        if hashsum_file(file_path) == md5:
+            print("File %s already exists at %s" % (local_filename,
+                file_path))
+            return file_path
+        else:
+            print("MD5 mismatch. Downloading again.")
 
+    print("Downloading %s to %s" % (local_filename, file_path))
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
     if os.path.exists(file_path):
@@ -43,6 +50,8 @@ def install_miniconda(path):
     from conda.utils import rm_rf
 
     prefix = os.path.join(tempdir, 'miniconda')
+    print("Installing Miniconda %s to %s" % (path, prefix))
+
     rm_rf(prefix)
     subprocess.call([path, '/S', '/D=%s' % prefix])
     subprocess.call([os.path.join(prefix, 'conda'), 'config', '--set', 'always_yes', 'yes'])
@@ -53,8 +62,8 @@ def main():
     for url, md5 in [
         ('http://repo.continuum.io/miniconda/Miniconda-3.5.5-Windows-x86_64.exe', 'b6285db92cc042a44b2afaaf1a99b8cc'),
         ]:
-        f = download_file(url)
-        hashsum_file(f)
+        f = download_file(url, md5)
+        install_miniconda(f)
 
 if __name__ == '__main__':
     main()
