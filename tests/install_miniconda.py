9f854cc7c54cdcce3de55212ed613c0288f19300
Aaron Meurer <asmeurer@gmail.com>
2014-12-11 11:13:55 -0600

    Apparently we can't import from conda, just copy rm_rf

--- tests/install_miniconda.py
+++ tests/install_miniconda.py
@@ -1,9 +1,63 @@
-import requests
+import sys
 import os
 import hashlib
 import subprocess
 
+import requests
+
 tempdir = os.path.expanduser("~")
+
+
+def rm_rf(path, max_retries=5):
+    """
+    Completely delete path
+
+    max_retries is the number of times to retry on failure. The default is
+    5. This only applies to deleting a directory.
+
+    """
+    import sys
+    import logging
+    import shutil
+    import stat
+    import time
+    from os.path import islink, isfile, isdir
+
+    on_win = bool(sys.platform == 'win32')
+    log = logging.getLogger(__name__)
+
+    if islink(path) or isfile(path):
+        # Note that we have to check if the destination is a link because
+        # exists('/path/to/dead-link') will return False, although
+        # islink('/path/to/dead-link') is True.
+        os.unlink(path)
+
+    elif isdir(path):
+        for i in range(max_retries):
+            try:
+                shutil.rmtree(path)
+                return
+            except OSError as e:
+                msg = "Unable to delete %s\n%s\n" % (path, e)
+                if on_win:
+                    try:
+                        def remove_readonly(func, path, excinfo):
+                            os.chmod(path, stat.S_IWRITE)
+                            func(path)
+                        shutil.rmtree(path, onerror=remove_readonly)
+                        return
+                    except OSError as e1:
+                        msg += "Retry with onerror failed (%s)\n" % e1
+
+                    try:
+                        subprocess.check_call(['cmd', '/c', 'rd', '/s', '/q', path])
+                        return
+                    except subprocess.CalledProcessError as e2:
+                        msg += '%s\n' % e2
+                log.debug(msg + "Retrying after %s seconds..." % i)
+                time.sleep(i)
+        # Final time. pass exceptions to caller.
+        shutil.rmtree(path)
 
 def download_file(url, md5):
     urlparts = requests.packages.urllib3.util.url.parse_url(url)
@@ -48,8 +102,6 @@ def hashsum_file(path, mode='md5'):
     return h.hexdigest()
 
 def install_miniconda(path):
-    from conda.install import rm_rf
-
     prefix = os.path.join(tempdir, 'conda-build-miniconda')
     print("Installing Miniconda %s to %s" % (path, prefix))
 
