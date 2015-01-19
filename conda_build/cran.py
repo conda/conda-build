f07c4f98d7a01442a5f874b0712dc5ca32007c2a
Aaron Meurer <asmeurer@gmail.com>
2015-01-19 16:33:08 -0600

    Use DYLD_FALLBACK_LIBRARY_PATH instead of DYLD_LIBRARY_PATH in skeleton cran
    Otherwise we hit the same issues we did with building R itself where system
    libraries that require older versions of libjpeg would pick up the newer
    version from Anaconda.

--- conda_build/cran.py
+++ conda_build/cran.py
@@ -79,8 +79,12 @@ mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 
 # On OS X, the only way to build packages currently is by having
-# DYLD_LIBRARY_PATH set.
-export DYLD_LIBRARY_PATH=$PREFIX/lib
+# DYLD_FALLBACK_LIBRARY_PATH set. We do not use DYLD_LIBRARY_PATH because that
+# screws up some of the system libraries that have older versions of libjpeg
+# than the one in Anaconda. DYLD_FALLBACK_LIBRARY_PATH will only come
+# into play if it cannot find the library via normal means. The default comes
+# from 'man dyld'.
+export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib:$(HOME)/lib:/usr/local/lib:/lib:/usr/lib
 
 R CMD INSTALL --build .
 
