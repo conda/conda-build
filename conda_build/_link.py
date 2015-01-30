6c42a98929372b89ccd34175d07b3c96258b4c07
Ilan Schnell <ilanschnell@gmail.com>
2015-01-30 15:01:18 -0600

    add linking site-packages files

--- conda_build/_link.py
+++ conda_build/_link.py
@@ -2,8 +2,8 @@ import os
 import sys
 import json
 import shutil
-from os.path import dirname, isdir, join
-
+from os.path import dirname, exists, isdir, join
+from distutils.sysconfig import get_python_lib
 
 
 THIS_DIR = dirname(__file__)
@@ -20,11 +20,51 @@ prefix = sys.prefix
 python = sys.executable
 
 
+def _link(src, dst):
+    try:
+        os.link(src, dst)
+        # on Windows os.link raises AttributeError
+    except (OSError, AttributeError):
+        shutil.copy2(src, dst)
+
+
 def _unlink(path):
     try:
         os.unlink(path)
     except OSError:
         pass
+
+
+def link_files(src_root, dst_root, files):
+    for f in files:
+        src = join(src_root, f)
+        dst = join(dst_root, f)
+        dst_dir = dirname(dst)
+        if not isdir(dst_dir):
+            os.makedirs(dst_dir)
+        if exists(dst):
+            _unlink(dst)
+        _link(src, dst)
+
+
+def unlink_files(root, files):
+    dst_dirs1 = set()
+    for f in files:
+        path = join(root, f)
+        _unlink(path)
+        dst_dirs1.add(dirname(path))
+
+    dst_dirs2 = set()
+    for path in dst_dirs1:
+        while len(path) > len(prefix):
+            dst_dirs2.add(path)
+            path = dirname(path)
+
+    for path in sorted(dst_dirs2, key=len, reverse=True):
+        try:
+            os.rmdir(path)
+        except OSError: # directory might not exist or not be empty
+            pass
 
 
 def create_entry_point(path, module, func):
@@ -68,6 +108,9 @@ def read_data():
 def link():
     d = read_data()
     create_entry_points(d['entry_points'])
+    link_files(join(THIS_DIR, 'site-packages'),
+               join(prefix, get_python_lib()),
+               d['site-packages'])
 
 
 def unlink():
