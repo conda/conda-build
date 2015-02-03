5c6f096ac22d17ccf467d938ef4f7950f0291a0a
Ilan Schnell <ilanschnell@gmail.com>
2015-02-03 00:06:16 -0600

    adding tests for function in _link module

--- conda_build/_link.py
+++ conda_build/_link.py
@@ -32,12 +32,12 @@ def _unlink(path):
         pass
 
 
-def pyc_f(f):
-    if sys.version_info[0] == 2:
+def pyc_f(f, version_info=sys.version_info):
+    if version_info[0] == 2:
         return f + 'c'
     dn, fn = f.rsplit('/', 1)
     return '%s/__pycache__/%s.cpython-%d%d.pyc' % (
-              dn, fn[:-3], sys.version_info[0], sys.version_info[1])
+              dn, fn[:-3], version_info[0], version_info[1])
 
 
 def link_files(src_root, dst_root, files):
