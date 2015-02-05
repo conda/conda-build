4ed1fb65a976b27ca0b4a536268ad83b9943242c
Aaron Meurer <asmeurer@gmail.com>
2015-02-05 11:51:35 -0600

    Don't set DYLD_FALLBACK_LIBRARY_PATH in cran recipes
    It is no longer needed on OS X with the new @rpath install name logic.

--- conda_build/cran.py
+++ conda_build/cran.py
@@ -80,14 +80,6 @@ CRAN_BUILD_SH = """\
 mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 
-# On OS X, the only way to build packages currently is by having
-# DYLD_FALLBACK_LIBRARY_PATH set. We do not use DYLD_LIBRARY_PATH because that
-# screws up some of the system libraries that have older versions of libjpeg
-# than the one in Anaconda. DYLD_FALLBACK_LIBRARY_PATH will only come
-# into play if it cannot find the library via normal means. The default comes
-# from 'man dyld'.
-export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib:$(HOME)/lib:/usr/local/lib:/lib:/usr/lib
-
 $R CMD INSTALL --build .
 
 # Add more build steps here, if they are necessary.
