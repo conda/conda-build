a521a9045da38bed3baf68139ed4448290fa1eda
Aaron Meurer <asmeurer@gmail.com>
2015-03-02 16:28:43 -0600

    Handle non-directories in copy_into
    Should fix #332.

--- conda_build/utils.py
+++ conda_build/utils.py
@@ -19,7 +19,10 @@ from conda.install import rm_rf
 def copy_into(src, dst):
     "Copy all the files and directories in src to the directory dst"
 
-    tocopy = os.listdir(src)
+    if not isdir(src):
+        tocopy = [src]
+    else:
+        tocopy = os.listdir(src)
     for afile in tocopy:
         srcname = os.path.join(src, afile)
         dstname = os.path.join(dst, afile)
