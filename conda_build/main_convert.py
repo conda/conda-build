531cc10e41fd4aef117ec64e87279bf216c0e65a
Aaron Meurer <asmeurer@gmail.com>
2015-04-22 17:29:17 -0500

    Update command docs for conda convert

--- conda_build/main_convert.py
+++ conda_build/main_convert.py
@@ -13,11 +13,11 @@ import re
 import sys
 import os
 import tarfile
-from argparse import RawDescriptionHelpFormatter
 from locale import getpreferredencoding
 from os.path import abspath, expanduser, isdir, join, split
 
 from conda.compat import PY3
+from conda.cli.conda_argparse import ArgumentParser
 from conda_build.main_build import args_func
 
 from conda_build.convert import (has_cext, tar_update, get_pure_py_file_map,
@@ -27,10 +27,9 @@ from conda_build.convert import (has_cext, tar_update, get_pure_py_file_map,
 epilog = """
 
 Tool to convert packages
-------------------------
 
-Convert pure Python packages to other platforms, and to convert Gohlke's
-.exe packages into conda packages.
+conda convert converts pure Python packages to other platforms, and converts
+Gohlke's .exe packages into conda packages.
 
 Packages are automatically organized in subdirectories according to platform,
 e.g.,
@@ -45,20 +44,24 @@ Examples:
 Convert a package built with conda build to Windows 64-bit, and place the
 resulting package in the current directory (supposing a default Anaconda
 install on Mac OS X):
-$ conda convert package-1.0-py33.tar.bz2 -p win-64
 
-Convert a .exe to a conda package, and add make it depend on numpy 1.8 or
-higher:
-$ conda convert cvxopt-1.1.7.win-amd64-py2.7.exe -d 'numpy >=1.8'
+    conda convert package-1.0-py33.tar.bz2 -p win-64
+
+Convert a Gohlke .exe to a conda package, and add make it depend on numpy 1.8
+or higher:
+
+    conda convert cvxopt-1.1.7.win-amd64-py2.7.exe -d 'numpy >=1.8'
 
 """
 
 
 def main():
-    p = argparse.ArgumentParser(
-        description='various tools to convert conda packages',
+    p = ArgumentParser(
+        description="""
+Various tools to convert conda packages. Takes a pure Python package build for
+one platform and converts it to work on one or more other platforms, or
+all.""" ,
         epilog=epilog,
-        formatter_class=RawDescriptionHelpFormatter,
     )
 
     # TODO: Factor this into a subcommand, since it's python package specific
@@ -67,55 +70,55 @@ def main():
         metavar='package-files',
         action="store",
         nargs='+',
-        help="package files to convert"
+        help="Package files to convert."
     )
     p.add_argument(
         '-p', "--platform",
         dest='platforms',
         action="append",
         choices=['osx-64', 'linux-32', 'linux-64', 'win-32', 'win-64', 'all'],
-        help="Platform to convert the packages to"
+        help="Platform to convert the packages to."
     )
     p.add_argument(
         "--dependencies", "-d",
         nargs='*',
         help="""Additional (besides python) dependencies of the converted
         package.  To specify a version restriction for a dependency, wrap
-        the dependency in quotes, like 'package >=2.0'""",
+        the dependency in quotes, like 'package >=2.0'.""",
     )
     p.add_argument(
         '--show-imports',
         action='store_true',
         default=False,
-        help="Show Python imports for compiled parts of the package",
+        help="Show Python imports for compiled parts of the package.",
     )
     p.add_argument(
         '-f', "--force",
         action="store_true",
-        help="Force convert, even when a package has compiled C extensions",
+        help="Force convert, even when a package has compiled C extensions.",
     )
     p.add_argument(
         '-o', '--output-dir',
         default='.',
         help="""Directory to write the output files. The packages will be
         organized in platform/ subdirectories, e.g.,
-        win-32/package-1.0-py27_0.tar.bz2"""
+        win-32/package-1.0-py27_0.tar.bz2."""
     )
     p.add_argument(
         '-v', '--verbose',
         default=False,
         action='store_true',
-        help="Print verbose output"
+        help="Print verbose output."
     )
     p.add_argument(
         "--dry-run",
         action="store_true",
-        help="only display what would have been done",
+        help="Only display what would have been done.",
     )
     p.add_argument(
         "-q", "--quiet",
         action="store_true",
-        help="Don't print as much output"
+        help="Don't print as much output."
     )
 
     p.set_defaults(func=execute)
