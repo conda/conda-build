fb10a3fbac0fdf1a856a327b1c291544db5f629f
Aaron Meurer <asmeurer@gmail.com>
2015-07-23 17:03:55 -0500

    Make always_include_files test run on Windows

--- tests/test-recipes/metadata/always_include_files_regex/run_test.py
+++ tests/test-recipes/metadata/always_include_files_regex/run_test.py
@@ -14,6 +14,8 @@ def main():
         assert sorted(info['files']) == [u'lib/libpng.dylib', u'lib/libpng16.16.dylib', u'lib/libpng16.dylib']
     elif sys.platform == 'linux2':
         assert sorted(info['files']) == ['lib/libpng.so', 'lib/libpng16.so', 'lib/libpng16.so.16', 'lib/libpng16.so.16.17.0']
+    elif sys.platform == 'win32':
+        assert sorted(info['files']) == [r'Library\lib\libpng.lib', r'Library\lib\libpng16.lib', r'Library\lib\libpng16_static.lib', r'libpng_static.lib']
 
 if __name__ == '__main__':
     main()
