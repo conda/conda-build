6f5ea53b6f529fc0e79cbfd9a2d6826a6657a4fd
Aaron Meurer <asmeurer@gmail.com>
2015-07-27 15:22:42 -0500

    Use Windows style environment variables

--- tests/test-recipes/metadata/python_run/run_test.bat
+++ tests/test-recipes/metadata/python_run/run_test.bat
@@ -1,4 +1,4 @@
 conda list -p "%PREFIX%" --canonical
 if errorlevel 1 exit 1
-conda list -p $PREFIX --canonical | grep "conda-build-test-python-run-1\.0-py.._0"
+conda list -p "%PREFIX%" --canonical | grep "conda-build-test-python-run-1\.0-py.._0"
 if errorlevel 1 exit 1
