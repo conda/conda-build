d5ed8650fc3d05144c177a58300c30701cf06d0a
Aaron Meurer <asmeurer@gmail.com>
2015-07-27 16:14:52 -0500

    Add git status debug output to test

--- tests/test-recipes/metadata/source_git/bld.bat
+++ tests/test-recipes/metadata/source_git/bld.bat
@@ -5,6 +5,8 @@ for /f "delims=" %%i in ('git describe') do set gitdesc=%%i
 if errorlevel 1 exit 1
 echo "%gitdesc%"
 if not "%gitdesc%"=="1.8.1" exit 1
+git status
+if errorlevel 1 exit 1
 set PYTHONPATH=.
 python -c "import conda_build; assert conda_build.__version__ == '1.8.1', conda_build.__version__"
 if errorlevel 1 exit 1
