8926df9265c945974ef6392ed1f74a59d3596ba3
Michael Sarahan <msarahan@continuum.io>
2015-09-23 13:33:51 -0500

    Move path prepending to function for uniformity.

--- conda_build/windows.py
+++ conda_build/windows.py
@@ -120,14 +120,12 @@ def build(m):
             data = fi.read()
         with open(join(src_dir, 'bld.bat'), 'w') as fo:
             fo.write(msvc_env_cmd())
-            for kv in iteritems(env):
-                fo.write('set "%s=%s"\n' % kv)
             # more debuggable with echo on
             fo.write('@echo on\n')
             fo.write("REM ===== end generated header =====\n")
             fo.write(data)
 
         cmd = [os.environ['COMSPEC'], '/c', 'call', 'bld.bat']
-        _check_call(cmd, cwd=src_dir)
+        _check_call(cmd, cwd=src_dir, env={str(k): str(v) for k, v in env.items()})
         kill_processes()
         fix_staged_scripts()
