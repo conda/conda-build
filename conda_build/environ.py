cfec3bc7809cbc4e9d7fa2621f4b48b012a5eae9
Stuart Berg <bergs@janelia.hhmi.org>
2015-11-01 23:09:25 -0500

    environ: Don't set NPY_VER at all if config.CONDA_NPY is undefined.

--- conda_build/environ.py
+++ conda_build/environ.py
@@ -89,7 +89,8 @@ def get_dict(m=None, prefix=None):
     d['SYS_PYTHON'] = sys.executable
     d['PERL_VER'] = get_perl_ver()
     d['PY_VER'] = get_py_ver()
-    d['NPY_VER'] = get_npy_ver()
+    if get_npy_ver():
+        d['NPY_VER'] = get_npy_ver()
     d['SRC_DIR'] = source.get_dir()
     if "LANG" in os.environ:
         d['LANG'] = os.environ['LANG']
