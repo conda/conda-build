01d786a6f106bd59bb8aa56ed018381722005743
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-11-30 13:55:07 -0500

    tests/test-recipes/metadata/source_git_jinja2/build.sh: Verify that `$PKG_VERSION` is correct, as well.

--- tests/test-recipes/metadata/source_git_jinja2/build.sh
+++ tests/test-recipes/metadata/source_git_jinja2/build.sh
@@ -4,4 +4,6 @@
 [ -d .git ]
 git describe
 [ "$(git describe)" = 1.8.1 ]
+echo "\$PKG_VERSION = $PKG_VERSION"
+[ "${PKG_VERSION}" = 1.8.1 ]
 PYTHONPATH=. python -c "import conda_build; assert conda_build.__version__ == '1.8.1', conda_build.__version__"
