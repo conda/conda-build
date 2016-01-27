07c28e19cc535dc02bb21ec76493420031312c8d
Ilan Schnell <ilanschnell@gmail.com>
2016-01-27 17:24:51 -0600

    add comment

--- conda_build/metadata.py
+++ conda_build/metadata.py
@@ -267,7 +267,8 @@ FIELDS = {
               'has_prefix_files', 'binary_has_prefix_files', 'script_env',
               'detect_binary_files_with_prefix', 'rpaths',
               'always_include_files', 'skip', 'msvc_compiler',
-              'pin_depends'],
+              'pin_depends' # pin_depends is experimental still
+             ],
     'requirements': ['build', 'run', 'conflicts'],
     'app': ['entry', 'icon', 'summary', 'type', 'cli_opts',
             'own_environment'],
