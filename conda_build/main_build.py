ad0a941e442e77f1d97768b6b5feec92136d1f41
Korijn van Golen <korijn@gmail.com>
2016-01-28 07:55:03 +0100

    Added explanatory comment

--- conda_build/main_build.py
+++ conda_build/main_build.py
@@ -276,6 +276,9 @@ def execute(args, parser):
             # used by jinja2. see https://github.com/conda/conda-build/pull/520
             delete_trash(None)
         except PermissionError:
+            # when we can't delete the trash, don't crash on AssertionError,
+            # instead inform the user and try again next time.
+            # see https://github.com/conda/conda-build/pull/744
             warnings.warn("Cannot delete trash; some c extension has been "
                           "imported that is hard-linked by files in the trash. "
                           "Will try again on next run.")
