c7b0f5b8eb03b99c50198aa7d0e9104d0612061d
Eric Dill <edill@bnl.gov>
2016-01-29 09:05:54 -0500

    MNT: Add GIT_* env vars for --output
    Add the capability for `--output` to respect GIT_* environmental
    variables.  Without this modification the GIT_* vars are not set, so
    conda-build /path/to/recipe --output returns an incorrect build string.

--- conda_build/main_build.py
+++ conda_build/main_build.py
@@ -361,6 +361,10 @@ def execute(args, parser):
                     print("%s is already built, skipping." % m.dist())
                     continue
             if args.output:
+                source.provide(m.path, m.get_section('source'))
+                # Parse our metadata again because we did not initialize the source
+                # information before.
+                m.parse_again()
                 print(build.bldpkg_path(m))
                 continue
             elif args.test:
