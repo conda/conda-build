d83419dd2bd581784e1afa92bcf98e56fafcba77
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-02-08 09:38:32 -0500

    Allow default values for environment variables only if specified.
    Instead of simply leaving undefined environment values as such. Provide
    a mechanism for giving them a default value in `meta.yaml`. This makes
    the breaking nature of this change less severe. Users requiring or
    desiring to set the environment variables to some default value when not
    picked up externally is possible. However, this is better than the
    original strategy as this does it in a manner that is explicit.

--- tests/test-recipes/metadata/build_env/run_test.py
+++ tests/test-recipes/metadata/build_env/run_test.py
@@ -1,9 +1,11 @@
 import os
 
 def main():
-    undef_var = os.environ.get("UNDEF_VAR")
+    undef_var1 = os.environ.get("UNDEF_VAR1")
+    undef_var2 = os.environ.get("UNDEF_VAR2")
 
-    assert undef_var is None
+    assert undef_var1 is None
+    assert undef_var2 == "UNDEFINED"
 
 if __name__ == '__main__':
     main()
