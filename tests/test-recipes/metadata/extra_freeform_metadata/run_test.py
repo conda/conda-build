efcd26c52e4d3eeecbda2126056cb012fbc1e14a
Ilan Schnell <ilanschnell@gmail.com>
2016-02-13 21:39:24 -0600

    update test

--- tests/test-recipes/metadata/extra_freeform_metadata/run_test.py
+++ tests/test-recipes/metadata/extra_freeform_metadata/run_test.py
@@ -1,5 +1,6 @@
 import os
 import json
+import yaml
 
 
 def main():
@@ -9,9 +10,10 @@ def main():
     with open(info_file, 'r') as fh:
         info = json.load(fh)
 
-    source_file = os.path.join(info['link']['source'], 'info', 'recipe.json')
+    source_file = os.path.join(info['link']['source'],
+                               'info', 'recipe', 'meta.yaml')
     with open(source_file, 'r') as fh:
-        source = json.load(fh)
+        source = yaml.load(fh)
 
     assert source['extra'] == {"custom": "metadata",
                                "however": {"we": "want"}}
