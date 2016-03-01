dcdfb516080902f63535e1732a5810b7a50a4295
Almar Klein <almar.klein@gmail.com>
2016-03-01 13:19:49 +0100

    add tests for noarch

---
+++ tests/test-recipes/metadata/noarch/noarch_test_package/noarch_test_package.py
@@ -0,0 +1,10 @@
+""" This functions as a module but also as entry point.
+"""
+
+answer = 142
+
+def main():
+    print(answer + 100)
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test-recipes/metadata/noarch/noarch_test_package/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+from distutils.core import setup
+
+name = 'noarch_test_package'
+
+setup(
+    name=name,
+    version='1.0',
+    author='Almar',
+    author_email='almar@notmyemail.com',
+    url='http://continuum.io',
+    license='(new) BSD',
+    description='testing noarch package building',
+    platforms='any',
+    provides=[name],
+    py_modules=[name],
+    entry_points={'console_scripts': ['%s_script = %s:main' % (name, name)], },
+)
