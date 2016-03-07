1d73b4f94573b3c4b96426a8e8113a8bd528c365
Ray Donnelly <mingw.android@gmail.com>
2016-03-07 12:05:41 +0000

    refactor: main_develop.sharedobjects_list => utils.rec_glob
    Wanted to perform some global queries on all dylibs in my Anaconda
    installations to get the id names and refactoring this seemed like
    an appropriate approach.
    
    Added macho.get_id(path) for the same end.

--- conda_build/main_develop.py
+++ conda_build/main_develop.py
@@ -8,15 +8,13 @@ from __future__ import absolute_import, division, print_function
 
 import sys
 from os.path import join, isdir, abspath, expanduser, exists
-from os import walk
-import fnmatch
 import shutil
 
 from conda.cli.common import add_parser_prefix, get_prefix
 from conda.cli.conda_argparse import ArgumentParser
 from conda_build.main_build import args_func
 from conda_build.post import mk_relative_osx
-from conda_build.utils import _check_call
+from conda_build.utils import _check_call, rec_glob
 
 from conda.install import linked
 
@@ -67,27 +65,6 @@ This works by creating a conda.pth file in site-packages."""
     args_func(args, p)
 
 
-def sharedobjects_list(pkg_path):
-    '''
-    return list of shared objects (*.so) found in pkg_path.
-
-    :param pkg_path: look for shared objects to relink in pkg_path
-    '''
-    bin_files = []
-
-    # only relevant for mac/linux
-    pattern = '*.so'
-
-    for d_f in walk(pkg_path):
-        m = fnmatch.filter(d_f[2], pattern)
-        if m:
-            # list is not empty, append full path to binary, then add it
-            # to bin_files list
-            bin_files.extend([join(d_f[0], f) for f in m])
-
-    return bin_files
-
-
 def relink_sharedobjects(pkg_path, build_prefix):
     '''
     invokes functions in post module to relink to libraries in conda env
@@ -101,7 +78,7 @@ def relink_sharedobjects(pkg_path, build_prefix):
         since runtime libraries should be loaded from environment's lib/. first
     '''
     # find binaries in package dir and make them relocatable
-    bin_files = sharedobjects_list(pkg_path)
+    bin_files = rec_glob(pkg_path, ['.so'])
     for b_file in bin_files:
         if sys.platform == 'darwin':
             mk_relative_osx(b_file, build_prefix)
