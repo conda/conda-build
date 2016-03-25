2800838377ae96f7f404341ffb17559d0b50e7f6
Ryan Grout <rgrout@continuum.io>
2016-03-25 17:27:54 -0500

    check GIT_* variables to make sure they are defined for git repos.

--- tests/test-recipes/metadata/source_path/build.sh
+++ tests/test-recipes/metadata/source_path/build.sh
@@ -1,2 +1,12 @@
 # If directory exists, we did it right
 cd tests/test-recipes/test-package
+
+# check if GIT_* variables are defined
+for i in GIT_DESCRIBE_TAG GIT_DESCRIBE_NUMBER GIT_DESCRIBE_HASH GIT_FULL_HASH
+do
+  if [ -n "eval $i" ]; then
+    eval echo \$$i
+  else
+    exit 1
+  fi
+done
