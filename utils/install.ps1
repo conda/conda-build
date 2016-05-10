9a5596505da39260e8b94aade3c5bd1038603799
Michael Sarahan <msarahan@gmail.com>
2016-05-10 12:23:21 -0500

    move appveyor conda update all to after setting PATH

--- utils/install.ps1
+++ utils/install.ps1
@@ -93,7 +93,6 @@ function InstallMinicondaPip ($python_home) {
 function main () {
     InstallMiniconda $env:PYTHON_VERSION $env:PYTHON_ARCH $env:PYTHON
     # InstallMinicondaPip $env:PYTHON
-    conda update --yes --all
 }
 
 main
