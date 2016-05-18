e3da205d20e6931524a4a0ee55cbb5378180c2b5
Michael Sarahan <msarahan@gmail.com>
2016-05-18 11:14:09 -0500

    refine double backslash finding; add test

---
+++ tests/test_build.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+from conda_build import build
+from conda.compat import TemporaryDirectory
+
+prefix_tests = {"normal": os.path.sep}
+if sys.platform == "win32":
+    prefix_tests.update({"double_backslash": "\\\\",
+                         "forward_slash": "/"})
+
+
+def _write_prefix(filename, prefix, replacement):
+    with open(filename, "w") as f:
+        f.write(prefix.replace(os.path.sep, replacement))
+        f.write("\n")
+
+
+def test_find_prefix_files():
+    # create a temporary folder
+    prefix = os.path.join(sys.prefix, "envs", "_build")
+    if not os.path.isdir(prefix):
+        os.makedirs(prefix)
+    with TemporaryDirectory(prefix=prefix + os.path.sep) as tmpdir:
+        # create text files to be replaced
+        files = []
+        for slash_style in prefix_tests:
+            filename = os.path.join(tmpdir, "%s.txt" % slash_style)
+            _write_prefix(filename, prefix, prefix_tests[slash_style])
+            files.append(filename)
+
+        assert len(list(build.have_prefix_files(files))) == len(files)
