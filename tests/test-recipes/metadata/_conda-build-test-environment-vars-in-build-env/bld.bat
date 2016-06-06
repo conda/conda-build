c0a8ca6681f65f2b17bf605fd07b7441a7885bdd
Michael Sarahan <msarahan@gmail.com>
2016-06-06 12:12:45 -0500

    add tests for running activation scripts in build env

---
+++ tests/test-recipes/metadata/_conda-build-test-environment-vars-in-build-env/bld.bat
@@ -0,0 +1,5 @@
+mkdir %PREFIX%\etc\conda\activate.d
+echo "set TEST_VAR=1" > %PREFIX%\etc\conda\activate.d\test.bat
+
+mkdir %PREFIX%\etc\conda\deactivate.d
+echo "set TEST_VAR=" > %PREFIX%\etc\conda\deactivate.d\test.bat
