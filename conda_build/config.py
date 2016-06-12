3916d261b5340eee010eb50a9439cd1fc4e4011c
Michael Sarahan <msarahan@gmail.com>
2016-06-12 18:04:09 -0500

    Revert "Update config.py"
    This reverts commit 5482880e12b9c4309d8cba1f71efae11717b9f72.

--- conda_build/config.py
+++ conda_build/config.py
@@ -58,7 +58,7 @@ class Config(object):
         croot = abspath(expanduser('~/conda-bld'))
 
     short_build_prefix = join(cc.envs_dirs[0], '_build')
-    long_build_prefix = max(short_build_prefix, (short_build_prefix + 8 * '_placehold')[:128])
+    long_build_prefix = max(short_build_prefix, (short_build_prefix + 8 * '_placehold')[:80])
     # XXX: Make this None to be more rigorous about requiring the build_prefix
     # to be known before it is used.
     use_long_build_prefix = False
