829eb861b90eba779071ddcb9d905cabf3877e47
Michael Sarahan <msarahan@gmail.com>
2016-06-23 08:07:04 -0500

    revert MSVC disutils changes

--- conda_build/windows.py
+++ conda_build/windows.py
@@ -1,15 +1,14 @@
 from __future__ import absolute_import, division, print_function
 
 import os
-import re
 import sys
 import shutil
-import subprocess
 from os.path import dirname, isdir, isfile, join
 
 # Leverage the hard work done by setuptools/distutils to find vcvarsall using
 # either the registry or the VS**COMNTOOLS environment variable
-from distutils.msvc9compiler import query_vcvarsall
+from distutils.msvc9compiler import find_vcvarsall as distutils_find_vcvarsall
+from distutils.msvc9compiler import Reg, WINSDK_BASE
 
 import conda.config as cc
 
@@ -21,6 +20,14 @@ from conda_build.utils import _check_call
 
 assert sys.platform == 'win32'
 
+WIN_SDK_71_PATH = Reg.get_value(os.path.join(WINSDK_BASE, 'v7.1'),
+                                'installationfolder')
+WIN_SDK_71_BAT_PATH = os.path.join(WIN_SDK_71_PATH, 'Bin', 'SetEnv.cmd')
+# Get the Visual Studio 2008 path (not the Visual C++ for Python path)
+# and get the 'vcvars64.bat' from inside the bin (in the directory above
+# that returned by distutils_find_vcvarsall)
+VCVARS64_VS9_BAT_PATH = os.path.join(os.path.dirname(distutils_find_vcvarsall(9)),
+                                     'bin', 'vcvars64.bat')
 VS_VERSION_STRING = {
     '8.0': 'Visual Studio 8 2005',
     '9.0': 'Visual Studio 9 2008',
@@ -65,20 +72,20 @@ def fix_staged_scripts():
 def msvc_env_cmd(bits, override=None):
     arch_selector = 'x86' if bits == 32 else 'amd64'
 
-    compiler_vars = {}
+    msvc_env_lines = []
 
     version = None
-    if override:
+    if override is not None:
         version = override
         # The DISTUTILS_USE_SDK variable tells distutils to not try and validate
         # the MSVC compiler. For < 3.5 this still forcibly looks for 'cl.exe'.
         # For > 3.5 it literally just skips the validation logic.
         # See distutils _msvccompiler.py and msvc9compiler.py / msvccompiler.py
         # for more information.
-        compiler_vars.update({"DISTUTILS_USE_SDK": 1,
-                              # This is also required to hit the 'don't validate' logic on < 3.5.
-                              # For > 3.5 this is ignored.
-                              "MSSdk": 1})
+        msvc_env_lines.append('set DISTUTILS_USE_SDK=1')
+        # This is also required to hit the 'don't validate' logic on < 3.5.
+        # For > 3.5 this is ignored.
+        msvc_env_lines.append('set MSSdk=1')
 
     if not version:
         if config.PY3K and config.use_MSVC2015:
@@ -88,20 +95,60 @@ def msvc_env_cmd(bits, override=None):
         else:
             version = '9.0'
 
-    compiler_vars.update({
-        "VS_VERSION": version,
-        "VS_MAJOR": version.split('.')[0],
-        "VS_YEAR": VS_VERSION_STRING[version][-4:],
-        "CMAKE_GENERATOR": VS_VERSION_STRING[version] + {64: ' Win64', 32: ''}[bits],
-        # tell msys2 to ignore path conversions for issue-causing windows-style flags in build
-        #   See https://github.com/conda-forge/icu-feedstock/pull/5
-        "MSYS2_ARG_CONV_EXCL": "/AI;/AL;/OUT;/out;%MSYS2_ARG_CONV_EXCL%",
-        "MSYS2_ENV_CONV_EXCL": "CL;%MSYS2_ENV_CONV_EXCL%",
-        })
+    vcvarsall_vs_path = build_vcvarsall_vs_path(version)
 
-    captured_vars = query_vcvarsall(float(version), arch_selector)
-    compiler_vars.update(captured_vars)
-    return compiler_vars
+    def build_vcvarsall_cmd(cmd, arch=arch_selector):
+        # Default argument `arch_selector` is defined above
+        return 'call "{cmd}" {arch}'.format(cmd=cmd, arch=arch)
+
+    msvc_env_lines.append('set "VS_VERSION={}"'.format(version))
+    msvc_env_lines.append('set "VS_MAJOR={}"'.format(version.split('.')[0]))
+    msvc_env_lines.append('set "VS_YEAR={}"'.format(VS_VERSION_STRING[version][-4:]))
+    msvc_env_lines.append('set "CMAKE_GENERATOR={}"'.format(VS_VERSION_STRING[version] +
+                                                            {64: ' Win64', 32: ''}[bits]))
+    # tell msys2 to ignore path conversions for issue-causing windows-style flags in build
+    #   See https://github.com/conda-forge/icu-feedstock/pull/5
+    msvc_env_lines.append('set "MSYS2_ARG_CONV_EXCL=/AI;/AL;/OUT;/out;%MSYS2_ARG_CONV_EXCL%"')
+    msvc_env_lines.append('set "MSYS2_ENV_CONV_EXCL=CL"')
+    if version == '10.0':
+        win_sdk_arch = '/Release /x86' if bits == 32 else '/Release /x64'
+        win_sdk_cmd = build_vcvarsall_cmd(WIN_SDK_71_BAT_PATH, arch=win_sdk_arch)
+
+        # There are two methods of building Python 3.3 and 3.4 extensions (both
+        # of which required Visual Studio 2010 - as explained in the Python wiki
+        # https://wiki.python.org/moin/WindowsCompilers)
+        # 1) Use the Windows SDK 7.1
+        # 2) Use Visual Studio 2010 (any edition)
+        # However, VS2010 never shipped with a 64-bit compiler, so in this case
+        # **only** option (1) applies. For this reason, we always try and
+        # activate the Windows SDK first. Unfortunately, unsuccessfully setting
+        # up the environment does **not EXIT 1** and therefore we must fall
+        # back to attempting to set up VS2010.
+        # DelayedExpansion is required for the SetEnv.cmd
+        msvc_env_lines.append('Setlocal EnableDelayedExpansion')
+        msvc_env_lines.append(win_sdk_cmd)
+        # If the WindowsSDKDir environment variable has not been successfully
+        # set then try activating VS2010
+        msvc_env_lines.append('if not "%WindowsSDKDir%" == "{}" ( {} )'.format(
+            WIN_SDK_71_PATH, build_vcvarsall_cmd(vcvarsall_vs_path)))
+    elif version == '9.0':
+        error1 = 'if errorlevel 1 {}'
+
+        # Setuptools captures the logic of preferring the Microsoft Visual C++
+        # Compiler for Python 2.7 - falls back to VS2008 if necessary
+        msvc_env_lines.append(build_vcvarsall_cmd(vcvarsall_vs_path))
+        # The Visual Studio 2008 Express edition does not properly contain
+        # the amd64 build files, so we call the vcvars64.bat manually,
+        # rather than using the vcvarsall.bat which would try and call the
+        # missing bat file.
+        if arch_selector == 'amd64':
+            msvc_env_lines.append(error1.format(
+                build_vcvarsall_cmd(VCVARS64_VS9_BAT_PATH)))
+    else:
+        # Visual Studio 14 or otherwise
+        msvc_env_lines.append(build_vcvarsall_cmd(vcvarsall_vs_path))
+
+    return '\n'.join(msvc_env_lines) + '\n'
 
 
 def kill_processes(process_names=["msbuild.exe"]):
@@ -126,18 +173,6 @@ def kill_processes(process_names=["msbuild.exe"]):
         except:
             continue
 
-def _merge_dicts(d1, d2):
-    """Merges d2's contents into d1.  Unlike update, this keeps all entries of both, by performing
-    unions of values."""
-    for key, value in d1.items():
-        if key in d2:
-            combined = set(value.split(';'))
-            combined.update(set(d2[key].split(';')))
-            d1[key] = ";".join(combined)
-            # delete it.  We'll merge remaining vars at the end.
-            del d2[key]
-    d1.update(d2)
-    return d1
 
 def build(m, bld_bat, dirty=False, activate=True):
     env = environ.get_dict(m, dirty=dirty)
@@ -154,23 +189,16 @@ def build(m, bld_bat, dirty=False, activate=True):
         with open(join(src_dir, 'bld.bat'), 'w') as fo:
             # more debuggable with echo on
             fo.write('@echo on\n')
-
-            compiler_vars = msvc_env_cmd(bits=cc.bits, override=m.get_value('build/msvc_compiler', None))
-            # ensure that all values are uppercase, for sake of merge.
-            env = {key.upper(): value for key, value in env.items()}
-            compiler_vars = {key.upper(): value for key, value in compiler_vars.items()}
-
-            # this is a union of all values from env and from compiler vars.  env should take priority.
-            env = _merge_dicts(env, compiler_vars)
-
             for key, value in env.items():
                 fo.write('set "{key}={value}"\n'.format(key=key, value=value))
             fo.write("set INCLUDE={};%INCLUDE%\n".format(env["LIBRARY_INC"]))
             fo.write("set LIB={};%LIB%\n".format(env["LIBRARY_LIB"]))
+            fo.write(msvc_env_cmd(bits=cc.bits, override=m.get_value('build/msvc_compiler', None)))
+            if activate:
+                fo.write("call activate _build\n")
             fo.write('\n')
             fo.write("REM ===== end generated header =====\n")
             fo.write(data)
-
 
         cmd = [os.environ['COMSPEC'], '/c', 'call', 'bld.bat']
         _check_call(cmd, cwd=src_dir)
