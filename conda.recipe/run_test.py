5a6a812c418be49db0b8571d09dfd1a089250bf3
Michael Sarahan <msarahan@gmail.com>
2016-06-27 12:54:43 -0500

    make conda.recipe run tests; make CIs use that

--- conda.recipe/run_test.py
+++ conda.recipe/run_test.py
@@ -1,7 +1,5 @@
 import os
-
-import pytest
 import conda_build
+import subprocess
 
 print('conda_build.__version__: %s' % conda_build.__version__)
-pytest.main([os.path.join(os.getenv("SOURCE_DIR"), 'tests')])
