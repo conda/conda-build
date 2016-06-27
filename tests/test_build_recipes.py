9c3970844cdbc24a96e5baa24465774f5596c1e3
Michael Sarahan <msarahan@gmail.com>
2016-06-27 15:21:38 -0500

    use pytest fixture for temp directories

--- tests/test_build_recipes.py
+++ tests/test_build_recipes.py
@@ -18,7 +18,7 @@ import pytest
 
 from conda_build.source import _guess_patch_strip_level, apply_patch
 
-from .utils import metadata_dir, is_valid_dir, fail_dir
+from .utils import metadata_dir, is_valid_dir, fail_dir, testing_workdir
 
 
 # def test_CONDA_BLD_PATH():
@@ -32,7 +32,7 @@ from .utils import metadata_dir, is_valid_dir, fail_dir
 
 
 # TODO: this does not currently take into account post-build versioning changes with __conda_? files
-def test_output_build_path_git_source():
+def test_output_build_path_git_source(testing_workdir):
     cmd = 'conda build --output {}'.format(os.path.join(metadata_dir, "source_git_jinja2"))
     process = subprocess.Popen(cmd.split(),
                     stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -54,47 +54,42 @@ def test_build_with_no_activate_does_not_activate():
 
 @pytest.mark.skipif(sys.platform == "win32",
                     reason="no binary prefix manipulation done on windows.")
-def test_binary_has_prefix_files():
+def test_binary_has_prefix_files(testing_workdir):
     cmd = 'conda build --no-anaconda-upload {}/_binary_has_prefix_files'.format(metadata_dir)
     subprocess.check_call(cmd.split())
 
 
 @pytest.mark.skipif(sys.platform == "win32",
                     reason="Windows permission errors w/ git when removing repo files on cleanup.")
-def test_cached_source_not_interfere_with_versioning():
+def test_cached_source_not_interfere_with_versioning(testing_workdir):
     """Test that work dir does not cache and cause inaccurate test target"""
-    basedir = os.getcwd()
     try:
-        with TemporaryDirectory() as tmp:
-            os.chdir(tmp)
-            subprocess.check_call(['git', 'clone',
-                                   'https://github.com/conda/conda_build_test_recipe'])
-            # build to make sure we have a work directory with source in it.
-            #    We want to make sure that whatever version that is does not
-            #    interfere with the test we run next.
-            subprocess.check_call(['conda', 'build', '--no-test',
-                                '--no-anaconda-upload',
-                                'conda_build_test_recipe'])
+        subprocess.check_call(['git', 'clone',
+                                'https://github.com/conda/conda_build_test_recipe'])
+        # build to make sure we have a work directory with source in it.
+        #    We want to make sure that whatever version that is does not
+        #    interfere with the test we run next.
+        subprocess.check_call(['conda', 'build', '--no-test',
+                            '--no-anaconda-upload',
+                            'conda_build_test_recipe'])
 
-            os.chdir('conda_build_test_recipe')
-            subprocess.check_call(['git', 'checkout', '1.20.0'])
-            os.chdir('..')
+        os.chdir('conda_build_test_recipe')
+        subprocess.check_call(['git', 'checkout', '1.20.0'])
+        os.chdir('..')
 
-            # this should fail, because we have not built v1.0, so there should
-            # be nothing to test.  If it succeeds, it means that it used the
-            # cached master checkout for determining which version to test.
-            cmd = 'conda build --output conda_build_test_recipe'
-            output = subprocess.check_output(cmd.split())
-            if PY3:
-                output = output.decode("UTF-8")
-            assert ("conda-build-test-source-git-jinja2-1.20.0" in output)
+        # this should fail, because we have not built v1.0, so there should
+        # be nothing to test.  If it succeeds, it means that it used the
+        # cached master checkout for determining which version to test.
+        cmd = 'conda build --output conda_build_test_recipe'
+        output = subprocess.check_output(cmd.split())
+        if PY3:
+            output = output.decode("UTF-8")
+        assert ("conda-build-test-source-git-jinja2-1.20.0" in output)
     except:
         raise
-    finally:
-        os.chdir(basedir)
 
 
-def test_relative_path_git_versioning():
+def test_relative_path_git_versioning(testing_workdir):
     tag = subprocess.check_output(["git", "describe", "--abbrev=0"]).rstrip()
     cmd = 'conda build --output {}'.format(os.path.join(metadata_dir,
                                                         "_source_git_jinja2_relative_path"))
@@ -102,7 +97,7 @@ def test_relative_path_git_versioning():
     assert tag in output
 
 
-def test_relative_git_url_git_versioning():
+def test_relative_git_url_git_versioning(testing_workdir):
     tag = subprocess.check_output(["git", "describe", "--abbrev=0"]).rstrip()
     cmd = 'conda build --output {}'.format(os.path.join(metadata_dir,
                                                         "_source_git_jinja2_relative_git_url"))
@@ -110,7 +105,7 @@ def test_relative_git_url_git_versioning():
     assert tag in output
 
 
-def test_package_test():
+def test_package_test(testing_workdir):
     """Test calling conda build -t <package file> - rather than <recipe dir>"""
     filename = "jinja2-2.8-py{}{}_0.tar.bz2".format(sys.version_info.major, sys.version_info.minor)
     downloaded_file = os.path.join(sys.prefix, 'conda-bld', subdir, filename)
@@ -123,16 +118,10 @@ def test_package_test():
 @pytest.fixture(params=[dirname for dirname in os.listdir(metadata_dir)
                         if is_valid_dir(metadata_dir, dirname)])
 def recipe(request):
-    cwd = os.getcwd()
-    os.chdir(metadata_dir)
-
-    def fin():
-        os.chdir(cwd)
-    request.addfinalizer(fin)
     return os.path.join(metadata_dir, request.param)
 
 
-def test_recipe_builds(recipe):
+def test_recipe_builds(recipe, testing_workdir):
     env = dict(os.environ)
     # These variables are defined solely for testing purposes,
     # so they can be checked within build scripts
@@ -148,7 +137,7 @@ def test_recipe_builds(recipe):
     subprocess.check_call(cmd.split(), env=env)
 
 
-def test_dirty_variable_available_in_build_scripts():
+def test_dirty_variable_available_in_build_scripts(testing_workdir):
     cmd = 'conda build --no-anaconda-upload --dirty {}'.format(os.path.join(metadata_dir,
                                                                     "_dirty_skip_section"))
     subprocess.check_call(cmd.split())
@@ -157,7 +146,7 @@ def test_dirty_variable_available_in_build_scripts():
         subprocess.check_call(cmd.split())
 
 
-def test_checkout_tool_as_dependency():
+def test_checkout_tool_as_dependency(testing_workdir):
     # "hide" svn by putting a known bad one on PATH
     tmpdir = tempfile.mkdtemp()
     dummyfile = os.path.join(tmpdir, "svn")
@@ -223,7 +212,7 @@ def test_build_msvc_compiler(msvc_ver):
 
 @pytest.mark.parametrize("platform", platforms)
 @pytest.mark.parametrize("target_compiler", compilers)
-def test_cmake_generator(platform, target_compiler):
+def test_cmake_generator(platform, target_compiler, testing_workdir):
     # TODO: need a better way to specify compiler more directly on win
     cmd = 'conda build --no-anaconda-upload {}/_cmake_generator --python={}'.\
           format(metadata_dir, target_compiler)
@@ -232,7 +221,7 @@ def test_cmake_generator(platform, target_compiler):
 
 @pytest.mark.skipif(sys.platform == "win32",
                     reason="No windows symlinks")
-def test_symlink_fail():
+def test_symlink_fail(testing_workdir):
     cmd = 'conda build --no-anaconda-upload {}'.format(os.path.join(fail_dir, "symlinks"))
     process = subprocess.Popen(cmd.split(),
                     stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -243,7 +232,7 @@ def test_symlink_fail():
 
 @pytest.mark.skipif(sys.platform == "win32",
                     reason="Windows doesn't show this error")
-def test_broken_conda_meta():
+def test_broken_conda_meta(testing_workdir):
     cmd = 'conda build --no-anaconda-upload {}'.format(os.path.join(fail_dir, "conda-meta"))
     process = subprocess.Popen(cmd.split(),
                     stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -252,7 +241,7 @@ def test_broken_conda_meta():
     assert "Error: Untracked file(s) ('conda-meta/nope',)" in error
 
 
-def test_recursive_fail():
+def test_recursive_fail(testing_workdir):
     cmd = 'conda build --no-anaconda-upload {}'.format(os.path.join(fail_dir, "recursive-build"))
     process = subprocess.Popen(cmd.split(),
                     stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -261,7 +250,7 @@ def test_recursive_fail():
     assert "recursive-build2" in error
 
 
-def test_jinja_typo():
+def test_jinja_typo(testing_workdir):
     cmd = 'conda build --no-anaconda-upload {}'.format(os.path.join(fail_dir,
                                                                     "source_git_jinja2_oops"))
     process = subprocess.Popen(cmd.split(),
@@ -271,7 +260,7 @@ def test_jinja_typo():
     assert "'GIT_DSECRIBE_TAG' is undefined" in error
 
 
-def test_skip_existing():
+def test_skip_existing(testing_workdir):
     # build the recipe first
     cmd = 'conda build --no-anaconda-upload {}'.format(os.path.join(metadata_dir, "empty_sections"))
     subprocess.check_output(cmd.split())
@@ -284,7 +273,7 @@ def test_skip_existing():
     assert "is already built, skipping." in output
 
 
-def test_token_upload():
+def test_token_upload(testing_workdir):
     # generated with conda_test_account user, command:
     #    anaconda auth --create --name CONDA_BUILD_UPLOAD_TEST --scopes 'api repos conda'
     token = "co-79de533f-926f-4e5e-a766-d393e33ae98f"
@@ -298,80 +287,71 @@ def test_token_upload():
 
 
 @pytest.mark.parametrize("service_name", ["binstar", "anaconda"])
-def test_no_anaconda_upload_condarc(service_name):
-    with TemporaryDirectory() as tmp:
-        rcfile = os.path.join(tmp, ".condarc")
-        with open(rcfile, 'w') as f:
-            f.write("{}_upload: False\n".format(service_name))
-        env = os.environ.copy()
-        env["CONDARC"] = rcfile
-        cmd = "conda build {}/empty_sections".format(metadata_dir)
-        process = subprocess.Popen(cmd.split(),
-                                   stdout=subprocess.PIPE,
-                                   stderr=subprocess.PIPE,
-                                   env=env)
-        output, error = process.communicate()
-        output = output.decode('utf-8')
-        error = error.decode('utf-8')
-        sys.stderr.write(output)
-        assert "Automatic uploading is disabled" in output, error
+def test_no_anaconda_upload_condarc(service_name, testing_workdir):
+    rcfile = os.path.join(testing_workdir, ".condarc")
+    with open(rcfile, 'w') as f:
+        f.write("{}_upload: False\n".format(service_name))
+    env = os.environ.copy()
+    env["CONDARC"] = rcfile
+    cmd = "conda build {}/empty_sections".format(metadata_dir)
+    process = subprocess.Popen(cmd.split(),
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE,
+                                env=env)
+    output, error = process.communicate()
+    output = output.decode('utf-8')
+    error = error.decode('utf-8')
+    sys.stderr.write(output)
+    assert "Automatic uploading is disabled" in output, error
 
 
-def test_patch_strip_level():
+def test_patch_strip_level(testing_workdir):
     patchfiles = set(('some/common/prefix/one.txt',
                       'some/common/prefix/two.txt',
                       'some/common/prefix/three.txt'))
     folders = ('some', 'common', 'prefix')
     files = ('one.txt', 'two.txt', 'three.txt')
-    basedir = os.getcwd()
-    with TemporaryDirectory() as tmp:
-        os.chdir(tmp)
-        os.makedirs(os.path.join(tmp, *folders))
-        for file in files:
-            with open(os.path.join(os.path.join(tmp, *folders), file), 'w') as f:
-                f.write('hello\n')
-        assert _guess_patch_strip_level(patchfiles, os.getcwd()) == 0
-        os.chdir(folders[0])
-        assert _guess_patch_strip_level(patchfiles, os.getcwd()) == 1
-        os.chdir(folders[1])
-        assert _guess_patch_strip_level(patchfiles, os.getcwd()) == 2
-        os.chdir(folders[2])
-        assert _guess_patch_strip_level(patchfiles, os.getcwd()) == 3
-        os.chdir(basedir)
+    os.makedirs(os.path.join(*folders))
+    for file in files:
+        with open(os.path.join(os.path.join(*folders), file), 'w') as f:
+            f.write('hello\n')
+    assert _guess_patch_strip_level(patchfiles, os.getcwd()) == 0
+    os.chdir(folders[0])
+    assert _guess_patch_strip_level(patchfiles, os.getcwd()) == 1
+    os.chdir(folders[1])
+    assert _guess_patch_strip_level(patchfiles, os.getcwd()) == 2
+    os.chdir(folders[2])
+    assert _guess_patch_strip_level(patchfiles, os.getcwd()) == 3
 
 
-def test_patch():
-    basedir = os.getcwd()
-    with TemporaryDirectory() as tmp:
-        os.chdir(tmp)
-        with open(os.path.join(tmp, 'file-deletion.txt'), 'w') as f:
-            f.write('hello\n')
-        with open(os.path.join(tmp, 'file-modification.txt'), 'w') as f:
-            f.write('hello\n')
-        patchfile = os.path.join(tmp, 'patch')
-        with open(patchfile, 'w') as f:
-            f.write('diff file-deletion.txt file-deletion.txt\n')
-            f.write('--- file-deletion.txt	2016-06-07 21:55:59.549798700 +0100\n')
-            f.write('+++ file-deletion.txt	1970-01-01 01:00:00.000000000 +0100\n')
-            f.write('@@ -1 +0,0 @@\n')
-            f.write('-hello\n')
-            f.write('diff file-creation.txt file-creation.txt\n')
-            f.write('--- file-creation.txt	1970-01-01 01:00:00.000000000 +0100\n')
-            f.write('+++ file-creation.txt	2016-06-07 21:55:59.549798700 +0100\n')
-            f.write('@@ -0,0 +1 @@\n')
-            f.write('+hello\n')
-            f.write('diff file-modification.txt file-modification.txt.new\n')
-            f.write('--- file-modification.txt	2016-06-08 18:23:08.384136600 +0100\n')
-            f.write('+++ file-modification.txt.new	2016-06-08 18:23:37.565136200 +0100\n')
-            f.write('@@ -1 +1 @@\n')
-            f.write('-hello\n')
-            f.write('+43770\n')
-            f.close()
-            apply_patch(tmp, patchfile)
-            assert not os.path.exists(os.path.join(tmp, 'file-deletion.txt'))
-            assert os.path.exists(os.path.join(tmp, 'file-creation.txt'))
-            assert os.path.exists(os.path.join(tmp, 'file-modification.txt'))
-            with open('file-modification.txt', 'r') as modified:
-                lines = modified.readlines()
-                assert lines[0] == '43770\n'
-        os.chdir(basedir)
+def test_patch(testing_workdir):
+    with open('file-deletion.txt', 'w') as f:
+        f.write('hello\n')
+    with open('file-modification.txt', 'w') as f:
+        f.write('hello\n')
+    patchfile = 'patch'
+    with open(patchfile, 'w') as f:
+        f.write('diff file-deletion.txt file-deletion.txt\n')
+        f.write('--- file-deletion.txt	2016-06-07 21:55:59.549798700 +0100\n')
+        f.write('+++ file-deletion.txt	1970-01-01 01:00:00.000000000 +0100\n')
+        f.write('@@ -1 +0,0 @@\n')
+        f.write('-hello\n')
+        f.write('diff file-creation.txt file-creation.txt\n')
+        f.write('--- file-creation.txt	1970-01-01 01:00:00.000000000 +0100\n')
+        f.write('+++ file-creation.txt	2016-06-07 21:55:59.549798700 +0100\n')
+        f.write('@@ -0,0 +1 @@\n')
+        f.write('+hello\n')
+        f.write('diff file-modification.txt file-modification.txt.new\n')
+        f.write('--- file-modification.txt	2016-06-08 18:23:08.384136600 +0100\n')
+        f.write('+++ file-modification.txt.new	2016-06-08 18:23:37.565136200 +0100\n')
+        f.write('@@ -1 +1 @@\n')
+        f.write('-hello\n')
+        f.write('+43770\n')
+        f.close()
+        apply_patch('.', patchfile)
+        assert not os.path.exists('file-deletion.txt')
+        assert os.path.exists('file-creation.txt')
+        assert os.path.exists('file-modification.txt')
+        with open('file-modification.txt', 'r') as modified:
+            lines = modified.readlines()
+        assert lines[0] == '43770\n'
--- tests/utils.py
+++ tests/utils.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+import pytest
+
 thisdir = os.path.dirname(os.path.realpath(__file__))
 metadata_dir = os.path.join(thisdir, "test-recipes/metadata")
 fail_dir = os.path.join(thisdir, "test-recipes/fail")
@@ -11,3 +13,24 @@ def is_valid_dir(parent_dir, dirname):
     valid &= not dirname.startswith("_")
     valid &= ('osx_is_app' != dirname or sys.platform == "darwin")
     return valid
+
+
+@pytest.fixture
+def testing_workdir(tmpdir, request):
+    """ Create a workdir in a safe temporary folder; cd into dir above before test, cd out after
+
+    :param tmpdir: py.test fixture, will be injected
+    :param request: py.test fixture-related, will be injected (see pytest docs)
+    """
+
+    saved_path = os.getcwd()
+
+    tmpdir.chdir()
+    workdir = tmpdir.mkdir('mysubdir')
+
+    def return_to_saved_path():
+        os.chdir(saved_path)
+
+    request.addfinalizer(return_to_saved_path)
+
+    return str(workdir)
