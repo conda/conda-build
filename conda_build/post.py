95b93114aa617ce921f020375c08e89feb09fc6e
Mike Sarahan <msarahan@gmail.com>
2016-08-09 16:25:39 -0500

    Merge pull request #1186 from msarahan/no_compile_exectuable_py
    don't compile .py files in executable locations

--- conda_build/post.py
+++ conda_build/post.py
@@ -1,27 +1,26 @@
 from __future__ import absolute_import, division, print_function
 
+from collections import defaultdict
+from glob import glob
+import io
 import locale
+import mmap
 import re
 import os
-import sys
-import stat
-from glob import glob
 from os.path import (basename, dirname, join, splitext, isdir, isfile, exists,
                      islink, realpath, relpath, normpath)
+import shutil
+import stat
+from subprocess import call
+import sys
 try:
     from os import readlink
 except ImportError:
     readlink = False
-import io
-import shutil
-from subprocess import call
-from collections import defaultdict
-import mmap
 
 from .conda_interface import lchmod
 from .conda_interface import walk_prefix
 from .conda_interface import md5_file
-from .conda_interface import PY3
 
 from conda_build.config import config
 from conda_build import external
@@ -181,13 +180,14 @@ def compile_missing_pyc(files, cwd=config.build_prefix, python_exe=config.build_
         else:
             if fn.startswith('bin'):
                 continue
-        compile_files.append(fn)
+        if fn.endswith(".py"):
+            compile_files.append(fn)
 
     if compile_files:
         print('compiling .pyc files...')
-        call([python_exe, '-Wi', '-m', 'py_compile'] + compile_files, cwd=cwd)
-        if PY3:
-            coerce_pycache_to_old_style(compile_files, cwd=cwd)
+        for f in compile_files:
+            call([python_exe, '-Wi', '-m', 'py_compile', f], cwd=cwd)
+        coerce_pycache_to_old_style(compile_files, cwd=cwd)
 
 
 def post_process(files, preserve_egg_dir=False):
