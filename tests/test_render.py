58065b07f41e0cfb6c789151d4c6eeab193a401c
Michael Sarahan <msarahan@gmail.com>
2016-08-17 10:20:21 -0500

    remove integration test in test_render.py

--- tests/test_render.py
+++ tests/test_render.py
@@ -10,15 +10,5 @@ thisdir = os.path.dirname(os.path.realpath(__file__))
 metadata_dir = os.path.join(thisdir, "test-recipes/metadata")
 
 
-def test_output_build_path():
-    cmd = 'conda render --output {}'.format(os.path.join(metadata_dir, "python_run"))
-    process = subprocess.Popen(cmd.split(),
-                    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, error = process.communicate()
-    test_path = os.path.join(sys.prefix, "conda-bld", subdir,
-                                  "conda-build-test-python-run-1.0-py{}{}_0.tar.bz2".format(
-                                      sys.version_info.major, sys.version_info.minor))
-    if PY3:
-        output = output.decode("UTF-8")
-        error = error.decode("UTF-8")
-    assert output.rstrip() == test_path, error
+# no tests here - this is tested at a high level in test_cli.py and in test_api_render.py.
+#   tests here should be lower-level unit tests of the render.py functionality.
