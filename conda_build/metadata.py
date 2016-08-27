bc123ef4de9316b4ed9737c283b2abd6d072a514
Michael Sarahan <msarahan@gmail.com>
2016-08-26 20:34:45 -0500

    add more informative message about pip reqs not allowed in meta.yaml

--- conda_build/metadata.py
+++ conda_build/metadata.py
@@ -556,6 +556,9 @@ class MetaData(object):
                 ms = MatchSpec(spec)
             except AssertionError:
                 raise RuntimeError("Invalid package specification: %r" % spec)
+            except AttributeError:
+                raise RuntimeError("Received dictionary as spec.  Note that pip requirements are not "
+                                   "supported in conda-build meta.yaml.")
             if ms.name == self.name():
                 raise RuntimeError("%s cannot depend on itself" % self.name())
             for name, ver in name_ver_list:
