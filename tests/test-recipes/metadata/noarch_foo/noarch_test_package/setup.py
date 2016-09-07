e834c17cd70ace5ba0b09473e22016ecf69924a2
sophia <scastellarin95@gmail.com>
2016-09-07 10:27:20 -0500

    Test noarch package building

---
+++ tests/test-recipes/metadata/noarch_foo/noarch_test_package/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+name = 'noarch_test_package'
+
+setup(
+    name=name,
+    version='1.0',
+    author='Almar',
+    author_email='almar@notmyemail.com',
+    url='http://continuum.io',
+    license='(new) BSD',
+    description='testing noarch package building',
+    platforms='any',
+    provides=[name],
+    py_modules=[name],
+    entry_points={'console_scripts': ['%s_script = %s:main' % (name, name)], },
+)
