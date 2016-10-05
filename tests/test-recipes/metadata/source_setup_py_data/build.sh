1128ea0f7134792dc282cb6802782c5eb8654096
Michael Sarahan <msarahan@gmail.com>
2016-10-05 09:18:49 -0500

    revert cython setup.py test

--- tests/test-recipes/metadata/source_setup_py_data/build.sh
+++ tests/test-recipes/metadata/source_setup_py_data/build.sh
@@ -3,6 +3,6 @@
 # Ensure we are in a git repo
 [ -d .git ]
 git describe
-[ "$(git describe)" = 1.21.5 ]
+[ "$(git describe)" = 1.21.0 ]
 echo "\$PKG_VERSION = $PKG_VERSION"
-[ "${PKG_VERSION}" = 1.21.5 ]
+[ "${PKG_VERSION}" = 1.21.0 ]
