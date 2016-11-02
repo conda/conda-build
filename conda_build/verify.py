71f1592278aee9dfc721793f5eedf6e7cf4a7ac9
sophia <scastellarin95@gmail.com>
2016-11-02 12:03:06 -0500

    Use scripts for verify from verify package

--- conda_build/verify.py
+++ conda_build/verify.py
@@ -3,18 +3,6 @@ from .exceptions import VerifyError
 import sys
 
 
-def can_import_importlib():
-    return sys.version_info.major >= 3 and sys.version_info.minor >= 3
-
-
-if can_import_importlib():
-    from importlib.machinery import SourceFileLoader
-else:
-    # imp.load_source has been deprecated in python3.3. So importlib should be used for versions
-    #   for versions of python greater than 3.3
-    import imp
-
-
 def list_verify_script(path_to_script):
     import pkgutil
     verify_modules = pkgutil.iter_modules([path_to_script])
@@ -29,22 +17,28 @@ def verify(verify_path, *args):
 
     for verify_script in verify_scripts:
         print("Running script %s" % verify_script)
-        if can_import_importlib():
-            mod = SourceFileLoader("test", verify_script).load_module()
-        else:
-            mod = imp.load_source("test", verify_script)
-        try:
-            mod.verify(*args)
-        except TypeError as e:
-            raise VerifyError(e, verify_script)
+
+
+        # try:
+        #     mod.verify(*args)
+        # except TypeError as e:
+        #     raise VerifyError(e, verify_script)
     print("All scripts passed")
 
 
 def verify_package(path_to_package, config):
-    verify_path = ""
-    verify(verify_path, path_to_package)
+    try:
+        verify_module = __import__("verify.package")
+    except ImportError as e:
+        print("can't find verify.package module, skipping verification")
+        return False
+    verify(verify_module.__path__, path_to_package)
 
 
 def verify_recipe(recipe, config):
-    verify_path = ""
-    verify(verify_path, recipe.meta, recipe.path)
+    try:
+        verify_module = __import__("verify.recipe")
+    except ImportError as e:
+        print("can't find verify.recipe module, skipping verification")
+        return False
+    verify(verify_module.__path__, recipe.meta, recipe.path)
