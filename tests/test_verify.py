2d3191d0febbd9a1a4634dfd419fde5489ac2e8d
sophia <scastellarin95@gmail.com>
2016-11-02 14:18:17 -0500

    Update tests

--- tests/test_verify.py
+++ tests/test_verify.py
@@ -8,30 +8,13 @@ class TestVerify(unittest.TestCase):
     def test_list_verify_script(self):
         path_to_scripts = join(dirname(__file__), "test-verify-scripts/package")
         files = verify.list_verify_script(path_to_scripts)
-        expected_scripts = [join(path_to_scripts, "test_package.py"),
-                            join(path_to_scripts, "test_package_2.py")]
+        expected_scripts = ["test_package", "test_package_2"]
         self.assertListEqual(files, expected_scripts)
 
         path_to_scripts = join(dirname(__file__), "test-verify-scripts/recipe")
         files = verify.list_verify_script(path_to_scripts)
-        expected_scripts = [join(path_to_scripts, "test_recipe.py")]
+        expected_scripts = ["test_recipe"]
         self.assertListEqual(files, expected_scripts)
 
-    def test_verify(self):
-        path_to_scripts = join(dirname(__file__), "test-verify-scripts/package")
-        verify.verify(path_to_scripts, "path_to_package_arg")
-
-        path_to_scripts = join(dirname(__file__), "test-verify-scripts/recipe")
-        verify.verify(path_to_scripts, "recipe_arg", "path_to_recipe_arg")
-
-    def test_verify_no_script(self):
-        path_to_script = "non/sense"
-        verify.verify(path_to_script)
-
-    def test_verify_error_script(self):
-        path_to_scripts = join(dirname(__file__), "test-verify-scripts/recipe")
-        try:
-            verify.verify(path_to_scripts, "recipe_arg")
-        except VerifyError as err:
-            self.assertTrue(err.error, TypeError)
-            self.assertEquals(err.script, join(path_to_scripts, "test_recipe.py"))
+    def test_no_verify_module(self):
+        self.assertFalse(verify.verify("nonsense", []))
