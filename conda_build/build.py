c7f3e2fd5554e12b27052799f1ccfe7e2aeaf641
sophia <scastellarin95@gmail.com>
2016-11-15 13:06:33 -0600

    Rename file_type method to node_type

--- conda_build/build.py
+++ conda_build/build.py
@@ -523,7 +523,7 @@ def get_inode_paths(files, target_short_path, prefix):
     return sorted(hardlinked_files)
 
 
-def file_type(path):
+def node_type(path):
     if islink(path):
         return NodeType.softlink
     return NodeType.hardlink
@@ -539,7 +539,7 @@ def build_info_files_json(m, prefix, files, files_with_prefix):
             "path": get_short_path(m, fi),
             "sha256": sha256_checksum(path),
             "size_in_bytes": os.path.getsize(path),
-            "node_type": file_type(path).name,
+            "node_type": node_type(path).name,
         }
         no_link = is_no_link(no_link, fi)
         if no_link:
