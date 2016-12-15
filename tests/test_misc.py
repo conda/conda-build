47b9136ae138d4403be3f3d3949c56f0d2dd5b86
sophia <scastellarin95@gmail.com>
2016-12-15 15:15:23 -0600

    Add test for st_nlink on win

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -3,8 +3,11 @@ import json
 from os import link
 from os.path import join
 
+import pytest
+from conda_build.utils import on_win
 import conda_build._link as _link
 from conda_build.conda_interface import PathType, EntityEncoder, CrossPlatformStLink
+
 
 class TestLink(unittest.TestCase):
 
@@ -43,6 +46,7 @@ def test_entity_encoder(tmpdir):
     assert json_file == {"a": "hardlink", "b": 1}
 
 
+@pytest.mark.skipif(on_win, reason="link not available on win/py2.7")
 def test_crossplatform_st_link(tmpdir):
     test_file = join(str(tmpdir), "test-file")
     test_file_linked = join(str(tmpdir), "test-file-linked")
@@ -54,4 +58,10 @@ def test_crossplatform_st_link(tmpdir):
     assert 1 == CrossPlatformStLink.st_nlink(test_file)
     assert 2 == CrossPlatformStLink.st_nlink(test_file_link)
     assert 2 == CrossPlatformStLink.st_nlink(test_file_linked)
+
+
+def test_crossplatform_st_link_on_win(tmpdir):
+    test_file = join(str(tmpdir), "test-file")
+    open(test_file, "a").close()
+    assert 1 == CrossPlatformStLink.st_nlink(test_file)
 
