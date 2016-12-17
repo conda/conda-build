517ae2e281bcdb4176101111816a1d430d114903
Aaron Meurer <asmeurer@gmail.com>
2016-12-16 19:22:13 -0500

    Only test that the work directory is empty
    The config wrapper might recreate it after it is deleted, so we only check
    that its contents have been cleared.

--- tests/test-recipes/work-deleted/test.py
+++ tests/test-recipes/work-deleted/test.py
@@ -5,4 +5,5 @@ PREFIX = os.environ["PREFIX"]
 with open(os.path.join(PREFIX, 'conda-build-test')) as f:
     SRC_DIR = f.read().strip()
 
-assert not os.path.exists(SRC_DIR), SRC_DIR
+# The directory might be recreated, so just check that it is empty
+assert not os.path.exists(SRC_DIR) or not os.listdir(SRC_DIR), SRC_DIR
