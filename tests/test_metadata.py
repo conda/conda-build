d07c8d6d3d9b5c383c91cb10a75949653a39e415
Michael Sarahan <msarahan@gmail.com>
2016-12-19 17:06:44 -0600

    disallow leading period in version

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -1,6 +1,8 @@
 import os
 import unittest
 
+import pytest
+
 from conda_build.conda_interface import MatchSpec
 
 from conda_build.metadata import select_lines, handle_config_version, expand_globs
@@ -108,3 +110,8 @@ class HandleConfigVersionTests(unittest.TestCase):
                           handle_config_version,
                           MatchSpec('numpy x.x'), None)
 
+
+def test_disallow_leading_period_in_version(test_metadata):
+    test_metadata.meta['package']['version'] = '.ste.ve'
+    with pytest.raises(AssertionError):
+        test_metadata.version()
