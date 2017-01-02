612aca5cd8a089f16016f66ab8474fb1207fbbbe
Michael Sarahan <msarahan@gmail.com>
2017-01-02 09:19:17 -0600

    fix no_link variable clobber.  Add test.

---
+++ tests/test-recipes/metadata/no_link/run_test.py
@@ -0,0 +1,5 @@
+import os
+f = os.path.join(os.getenv('PREFIX'), 'no_link_file.example')
+assert os.path.isfile(f), "File does not exist"
+assert os.stat(f).st_nlink == 1, "File is hard-linked where it should not be"
+assert not os.path.islink(f), "File is a symlink where it should not be"
