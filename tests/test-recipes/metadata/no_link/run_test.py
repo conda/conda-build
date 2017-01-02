88852cc44f687e8fe85eaed31a4ee9a02c46f49c
Michael Sarahan <msarahan@gmail.com>
2017-01-02 16:17:20 -0600

    fix no_link test on win py2.7, link count = 0

--- tests/test-recipes/metadata/no_link/run_test.py
+++ tests/test-recipes/metadata/no_link/run_test.py
@@ -1,5 +1,5 @@
 import os
 f = os.path.join(os.getenv('PREFIX'), 'no_link_file.example')
 assert os.path.isfile(f), "File does not exist"
-assert os.stat(f).st_nlink == 1, "File is hard-linked where it should not be"
+assert os.stat(f).st_nlink <= 1, "File is hard-linked where it should not be (link count = {})".format(os.stat(f).st_nlink)
 assert not os.path.islink(f), "File is a symlink where it should not be"
