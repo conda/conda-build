db84797c377f3d7dcca5f2a3cbd1c5d22608b11e
Michael Sarahan <msarahan@gmail.com>
2017-02-28 11:17:04 -0600

    fix symlinks to folders in packaging

--- tests/test-recipes/metadata/symlinks/run_test.sh
+++ tests/test-recipes/metadata/symlinks/run_test.sh
@@ -2,7 +2,15 @@
 sh $PREFIX/test/weee
 
 # the symlink to the file
-sh $PREFIX/test/weee
+sh $PREFIX/bin/weee
+if [ ! -L $PREFIX/bin/weee ]; then
+    echo "file in package is not a symlink"
+    exit 1
+fi
 
 # the symlink to the folder
 sh $PREFIX/test_link/weee
+if [ ! -L $PREFIX/test_link ]; then
+    echo "folder in package is not a symlink"
+    exit 1
+fi
