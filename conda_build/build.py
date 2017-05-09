8a103a0fc92aa257403b4f3b8d0c0f82eb5b5a4c
Michael Sarahan <msarahan@gmail.com>
2017-05-09 14:35:33 -0500

    changelog 2.1.11

--- conda_build/build.py
+++ conda_build/build.py
@@ -810,7 +810,8 @@ def post_process_files(m, initial_prefix_files):
     get_build_metadata(m, config=m.config)
     create_post_scripts(m, config=m.config)
 
-    # this is new-style noarch, with a value of 'python'
+    # this is new-style noarch, with a value of 'python'. We handle entry
+    # points for that at install time
     if not is_noarch_python(m):
         utils.create_entry_points(m.get_value('build/entry_points'), config=m.config)
     current_prefix_files = prefix_files(prefix=m.config.build_prefix)
