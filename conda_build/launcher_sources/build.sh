b700b8b73f134de249806668371e83e52729c380
Ray Donnelly <mingw.android@gmail.com>
2017-05-18 11:46:35 +0100

    Fix {cli,gui}-{32,64}.exe for spaces in the final exe path

--- conda_build/launcher_sources/build.sh
+++ conda_build/launcher_sources/build.sh
@@ -20,7 +20,7 @@ for _BITS in 64 32; do
     # You *could* use MSVC 2008 here, but you'd end up with much larger (~230k) executables.
     # cl.exe -opt:nowin98 -D NDEBUG -D "GUI=0" -D "WIN32_LEAN_AND_MEAN" -ZI -Gy -MT -MERGE launcher.c -Os -link -MACHINE:x64 -SUBSYSTEM:CONSOLE version.lib advapi32.lib shell32.lib
     PATH=/mingw${_BITS}/bin:$PATH gcc -O2 -DSCRIPT_WRAPPER -DUNICODE -D_UNICODE -DMINGW_HAS_SECURE_API ${CPPFLAGS} launcher.c -c -o ${_TYPE}-${_BITS}.o
-    PATH=/mingw${_BITS}/bin:$PATH gcc -Wl,-s --static -static-libgcc -municode ${LDFLAGS} ${_TYPE}-${_BITS}.o resources-${_BITS}.res -lversion -o ${_TYPE}-${_BITS}.exe
+    PATH=/mingw${_BITS}/bin:$PATH gcc -Wl,-s --static -static-libgcc -municode ${LDFLAGS} ${_TYPE}-${_BITS}.o resources-${_BITS}.res -o ${_TYPE}-${_BITS}.exe
   done
 done
 ls -l *.exe
