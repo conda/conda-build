e6b30ca5f6ef3a0603eaa10b92cd28c33dda85d3
Michael Sarahan <msarahan@gmail.com>
2017-06-26 14:42:12 -0500

    remove strict need to always use jinja2 for variants

--- tests/test_render.py
+++ tests/test_render.py
@@ -1,5 +1,6 @@
 import os
 from conda_build import api
+from conda_build import render
 
 
 def test_output_with_noarch_says_noarch(testing_metadata):
@@ -14,5 +15,13 @@ def test_output_with_noarch_python_says_noarch(testing_metadata):
     assert os.path.sep + "noarch" + os.path.sep in output[0]
 
 
-# no tests here - this is tested at a high level in test_cli.py and in test_api_render.py.
-#   tests here should be lower-level unit tests of the render.py functionality.
+def test_insert_variant_versions(testing_metadata):
+    testing_metadata.meta['requirements']['build'] = ['python', 'numpy 1.13']
+    testing_metadata.config.variant = {'python': '2.7', 'numpy': '1.11'}
+    render.insert_variant_versions(testing_metadata, 'build')
+    # this one gets inserted
+    assert 'python 2.7' in testing_metadata.meta['requirements']['build']
+    # this one should not be altered
+    assert 'numpy 1.13' in testing_metadata.meta['requirements']['build']
+    # the overall length does not change
+    assert len(testing_metadata.meta['requirements']['build']) == 2
