5426697876d0860bf1451247bd9ab849d78e17ae
Michael Sarahan <msarahan@gmail.com>
2017-08-29 12:05:13 -0500

    fix zip_keys becoming a loop dimension.  fix variants from earlier builds affecting later

--- conda_build/build.py
+++ conda_build/build.py
@@ -1709,12 +1709,12 @@ def build_tree(recipe_list, config, build_only=False, post=False, notest=False,
                 recipe_parent_dir = os.path.dirname(metadata.path)
                 to_build_recursive.append(metadata.name())
 
-                variants = (dict_of_lists_to_list_of_dicts(variants) if variants else
+                variants_ = (dict_of_lists_to_list_of_dicts(variants) if variants else
                             get_package_variants(metadata))
 
                 # This is where reparsing happens - we need to re-evaluate the meta.yaml for any
                 #    jinja2 templating
-                metadata_tuples = distribute_variants(metadata, variants,
+                metadata_tuples = distribute_variants(metadata, variants_,
                                                       permit_unsatisfiable_variants=False)
             else:
                 recipe_parent_dir = os.path.dirname(recipe)
