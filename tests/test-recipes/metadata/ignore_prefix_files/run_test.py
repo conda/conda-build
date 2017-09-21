f69a3d833711700bbd7d3d95317ff161e5ec771b
Kale Franz <kfranz@continuum.io>
2017-09-21 02:02:57 -0500

    prepare 2.1.x tests for enhanced package cache structure

--- tests/test-recipes/metadata/ignore_prefix_files/run_test.py
+++ tests/test-recipes/metadata/ignore_prefix_files/run_test.py
@@ -1,6 +1,16 @@
 import os
 
 pkgs = os.path.join(os.environ["ROOT"], "pkgs")
-info_dir = os.path.join(pkgs, "conda-build-test-ignore-prefix-files-1.0-0", "info")
+package_dir_name = "conda-build-test-ignore-prefix-files-1.0-0"
+info_dir = os.path.join(pkgs, package_dir_name, "info")
+
+if not os.path.isdir(info_dir):
+    channel_name = os.path.basename(os.path.dirname(os.path.dirname(os.environ["PREFIX"])))
+    print("channel_name: %s" % channel_name)
+    info_dir = os.path.join(pkgs, channel_name, os.environ["SUBDIR"], package_dir_name, "info")
+    print(info_dir)
+    if not os.path.isdir(info_dir):
+        raise RuntimeError("No info_dir found: %s" % info_dir)
+
 assert os.path.isdir(info_dir)
 assert not os.path.isfile(os.path.join(info_dir, "has_prefix"))
--- tests/test-recipes/metadata/ignore_some_prefix_files/run_test.py
+++ tests/test-recipes/metadata/ignore_some_prefix_files/run_test.py
@@ -1,9 +1,19 @@
 import os
 
 pkgs = os.path.join(os.environ["ROOT"], "pkgs")
-info_dir = os.path.join(pkgs, "conda-build-test-ignore-some-prefix-files-1.0-0", "info")
-has_prefix_file = os.path.join(info_dir, "has_prefix")
+package_dir_name = "conda-build-test-ignore-some-prefix-files-1.0-0"
+info_dir = os.path.join(pkgs, package_dir_name, "info")
 print(info_dir)
+
+if not os.path.isdir(info_dir):
+    channel_name = os.path.basename(os.path.dirname(os.path.dirname(os.environ["PREFIX"])))
+    print("channel_name: %s" % channel_name)
+    info_dir = os.path.join(pkgs, channel_name, os.environ["SUBDIR"], package_dir_name, "info")
+    print(info_dir)
+    if not os.path.isdir(info_dir):
+        raise RuntimeError("No info_dir found: %s" % info_dir)
+
+has_prefix_file = os.path.join(info_dir, "has_prefix")
 assert os.path.isfile(has_prefix_file)
 with open(has_prefix_file) as f:
     assert "test2" not in f.read()
