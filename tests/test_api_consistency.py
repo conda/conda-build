7550fdfb11cbc70eab402c21ab54a11e3fd654fb
Michael Sarahan <msarahan@gmail.com>
2017-10-12 10:09:12 -0500

    flake8; rearrange nested function

--- tests/test_api_consistency.py
+++ tests/test_api_consistency.py
@@ -119,4 +119,4 @@ def test_api_create_metapackage():
 def test_api_update_index():
     argspec = getargspec(api.update_index)
     assert argspec.args == ['dir_paths', 'config', 'force', 'check_md5', 'remove', 'channel_name']
-    assert argspec.defaults == (None, False, False, False)
+    assert argspec.defaults == (None, False, False, False, None)
