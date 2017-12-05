2f089856a0a514c61e42692109d0c6595abcc7a6
Michael Sarahan <msarahan@gmail.com>
2017-12-05 09:53:28 -0600

    don't filter out all files in info dir when checking tar

--- conda_build/utils.py
+++ conda_build/utils.py
@@ -1002,6 +1002,29 @@ def filter_files(files_list, prefix, filter_patterns=('(.*[\\\\/])?\.git[\\\\/].
             os.path.islink(os.path.join(prefix, f))]
 
 
+def filter_info_files(files_list, prefix):
+    return filter_files(files_list, prefix, filter_patterns=(
+                    'info/index.json',
+                    'info/files',
+                    'info/paths.json',
+                    'info/about.json',
+                    'info/has_prefix',
+                    'info/hash_input_files',   # legacy, not used anymore
+                    'info/hash_input.json',
+                    'info/run_exports.yaml',
+                    'info/git',
+                    'info/recipe/.*',
+                    'info/recipe.tar',
+                    'info/test/.*',
+                    'info/LICENSE.txt',
+                    'info/requires',
+                    'info/meta',
+                    'info/platform',
+                    'info/no_link',
+                    'info/link.json',
+            ))
+
+
 def rm_rf(path, config=None):
     if on_win:
         # native windows delete is potentially much faster
