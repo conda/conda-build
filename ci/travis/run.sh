b937c0be332bf1de8ec021722403ecaad8574fe6
Michael Sarahan <msarahan@gmail.com>
2017-12-18 11:46:04 -0600

    CI improvements for build speed and avoiding race conditions

--- ci/travis/run.sh
+++ ci/travis/run.sh
@@ -2,7 +2,7 @@
 set -ev
 if [[ "$FLAKE8" == "true" ]]; then
     flake8 .
-    cp bdist_conda.py $HOME/miniconda/lib/python${TRAVIS_PYTHON_VERSION}/distutils/command
+    cp bdist_conda.py /opt/conda/lib/python${TRAVIS_PYTHON_VERSION}/distutils/command
     pushd tests/bdist-recipe && python setup.py bdist_conda && popd
     conda build --help
     conda build --version
@@ -13,6 +13,14 @@ if [[ "$FLAKE8" == "true" ]]; then
     source activate _cbtest
     conda build conda.recipe --no-anaconda-upload
 else
-    $HOME/miniconda/bin/py.test -v -n 0 --basetemp /tmp/cb --cov conda_build --cov-report xml -m "serial" tests
-    $HOME/miniconda/bin/py.test -v -n 2 --basetemp /tmp/cb --cov conda_build --cov-append --cov-report xml -m "not serial" tests --forked
+    echo "safety_checks: disabled" >> ~/.condarc
+    echo "local_repodata_ttl: 1800" >> ~/.condarc
+    mkdir -p ~/.conda
+    conda create -n blarg1 -yq python=2.7
+    conda create -n blarg2 -yq python=3.4
+    conda create -n blarg3 -yq python=3.5
+    conda create -n blarg4 -yq python=3.6
+    conda create -n blarg5 -yq libpng=1.6.17
+    /opt/conda/bin/py.test -v -n 0 --basetemp /tmp/cb --cov conda_build --cov-report xml -m "serial" tests
+    /opt/conda/bin/py.test -v -n 2 --basetemp /tmp/cb --cov conda_build --cov-append --cov-report xml -m "not serial" tests --forked
 fi
