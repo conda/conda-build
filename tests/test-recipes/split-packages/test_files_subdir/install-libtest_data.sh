45688644ac6cedd7b501394226a09719a0028b01
Ray Donnelly <mingw.android@gmail.com>
2018-05-08 16:18:34 +0100

    Add failing test showing problem reusing same recipe test data twice
    ```
    During handling of the above exception, another exception occurred:
    
    Traceback (most recent call last):
      File "/opt/conda/bin/conda-build", line 11, in <module>
        load_entry_point('conda-build', 'console_scripts', 'conda-build')()
      File "/Users/rdonnelly/conda/conda-build/conda_build/cli/main_build.py", line 420, in main
        execute(sys.argv[1:])
      File "/Users/rdonnelly/conda/conda-build/conda_build/cli/main_build.py", line 411, in execute
        verify=args.verify)
      File "/Users/rdonnelly/conda/conda-build/conda_build/api.py", line 200, in build
        notest=notest, need_source_download=need_source_download, variants=variants)
      File "/Users/rdonnelly/conda/conda-build/conda_build/build.py", line 2143, in build_tree
        test(pkg, config=metadata.config, stats=stats)
      File "/Users/rdonnelly/conda/conda-build/conda_build/build.py", line 1800, in test
        copy_test_source_files(metadata, metadata.config.test_dir)
      File "/Users/rdonnelly/conda/conda-build/conda_build/build.py", line 360, in copy_test_source_files
        create_all_test_files(m, destination)
      File "/Users/rdonnelly/conda/conda-build/conda_build/create_test.py", line 232, in create_all_test_files
        files = create_files(m, test_dir)
      File "/Users/rdonnelly/conda/conda-build/conda_build/create_test.py", line 37, in create_files
        clobber=True)
      File "/Users/rdonnelly/conda/conda-build/conda_build/utils.py", line 466, in copy_into
        _copy_with_shell_fallback(src, dst_fn)
      File "/Users/rdonnelly/conda/conda-build/conda_build/utils.py", line 399, in _copy_with_shell_fallback
        raise OSError("Failed to copy {} to {}.  Error was: {}".format(src, dst, e))
    ```

---
+++ tests/test-recipes/split-packages/test_files_subdir/install-libtest_data.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+# Ensure files from non-dependencies are *not* present.
+if [[ -f ${PREFIX}/lib/lib2intradependencies.so ]]; then
+  echo "ERROR: ${PREFIX}/lib/lib2intradependencies.so found during install of ${PKG_NAME} but it is not a dependency"
+  exit 1
+fi
+if [[ -f ${PREFIX}/bin/py2-intradependencies ]]; then
+  echo "ERROR: ${PREFIX}/bin/py2-intradependencies found during install of ${PKG_NAME} but it is not a dependency"
+  exit 1
+fi
+if [[ -f ${PREFIX}/bin/r2-intradependencies ]]; then
+  echo "ERROR: ${PREFIX}/bin/r2-intradependencies found during install of ${PKG_NAME} but it is not a dependency"
+  exit 1
+fi
+
+if [[ ! -d ${PREFIX}/lib ]]; then
+  mkdir -p ${PREFIX}/lib
+fi
+
+echo lib1intradependencies.so > ${PREFIX}/lib/lib1intradependencies.so
---
+++ tests/test-recipes/split-packages/test_files_subdir/install-test_data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo install-test_data.sh
