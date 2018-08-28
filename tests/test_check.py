763d1c17a017c9bc551b1a8feb197e2244228fc5
Teake Nutma <t.a.nutma@rug.nl>
2018-08-28 14:32:14 +0200

    Fix --check for optionally iterable fields
    `source` and `outputs` can contain a list of dictionaries,
    in which case checking would fail.

---
+++ tests/test_check.py
@@ -0,0 +1,9 @@
+import os
+
+from conda_build import api
+from .utils import metadata_dir
+
+
+def test_check_multiple_sources():
+    recipe = os.path.join(metadata_dir, 'multiple_sources')
+    assert api.check(recipe)
