152827e4a293f39528477fb62bccb7c9007abe0e
James Sexton <TwistedLadder@gmail.com>
2018-10-22 16:06:29 -0400

    Update build.py

--- conda_build/build.py
+++ conda_build/build.py
@@ -1817,8 +1817,11 @@ def _construct_metadata_for_test_from_package(package, config):
     # HACK: because the recipe is fully baked, detecting "used" variables no longer works.  The set
     #     of variables in the hash_input suffices, though.
     metadata.config.used_vars = list(hash_input.keys())
-    metadata.config.channel_urls = list(utils.ensure_list(metadata.config.channel_urls))
-    metadata.config.channel_urls.insert(0, url_path(local_channel))
+    urls = list(utils.ensure_list(metadata.config.channel_urls))
+    if 'local' not in urls:
+        urls.insert(0, 'local')
+    # replace local with the appropriate real channel.  Order is maintained.
+    metadata.config.channel_urls = [url if url != 'local' else url_path(local_channel) for url in urls]
     return metadata, hash_input
 
 
