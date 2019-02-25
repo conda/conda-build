f80bbb09ff2e6f01bbd6564c642e61b509fc68f0
Sophia Castellarin <scastellarin@anaconda.com>
2019-02-25 09:28:15 -0600

    Add build for docs on travis

--- ci/travis/run.sh
+++ ci/travis/run.sh
@@ -13,16 +13,25 @@ if [[ "$FLAKE8" == "true" ]]; then
     conda install -n _cbtest $(conda render --output conda.recipe | head -n 1)
     source activate _cbtest
     conda build conda.recipe --no-anaconda-upload
+elif [[ "$DOCS" == "true" ]]; then
+    cd docs
+    make html
 else
-    echo "safety_checks: disabled" >> ~/.condarc
-    echo "local_repodata_ttl: 1800" >> ~/.condarc
-    mkdir -p ~/.conda
-    conda create -n blarg1 -yq python=2.7
-    conda create -n blarg3 -yq python=3.5
-    conda create -n blarg4 -yq python nomkl numpy pandas svn
-    conda create -n blarg5 -yq libpng=1.6.17
-    /opt/conda/bin/py.test -v -n 2 --basetemp /tmp/cb --cov conda_build --cov-append --cov-report xml -m "not serial" tests --forked
-    # install conda-verify from its master branch, at least for a while until it's more stable
-    pip install git+https://github.com/conda/conda-verify.git
-    /opt/conda/bin/py.test -v -n 0 --basetemp /tmp/cb --cov conda_build --cov-report xml -m "serial" tests
+    FILES_CHANGED=$(git status --porcelain | wc -l)
+    DOCS_FILES_CHANGED=$(git status --porcelain | grep docs | wc -l)
+    if [[ $FILES_CHANGED == $DOCS_FILES_CHANGED ]]; then
+      echo "Only docs changed detected, skipping tests"
+    else
+      echo "safety_checks: disabled" >> ~/.condarc
+      echo "local_repodata_ttl: 1800" >> ~/.condarc
+      mkdir -p ~/.conda
+      conda create -n blarg1 -yq python=2.7
+      conda create -n blarg3 -yq python=3.5
+      conda create -n blarg4 -yq python nomkl numpy pandas svn
+      conda create -n blarg5 -yq libpng=1.6.17
+      /opt/conda/bin/py.test -v -n 2 --basetemp /tmp/cb --cov conda_build --cov-append --cov-report xml -m "not serial" tests --forked
+      # install conda-verify from its master branch, at least for a while until it's more stable
+      pip install git+https://github.com/conda/conda-verify.git
+      /opt/conda/bin/py.test -v -n 0 --basetemp /tmp/cb --cov conda_build --cov-report xml -m "serial" tests
+    fi
 fi
