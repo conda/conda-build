49d32d7a75705d933319266853ffee670f38f236
Sophia Castellarin <scastellarin@anaconda.com>
2019-03-13 10:01:21 -0500

    Make build look more like conda

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,9 +15,10 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../..'))
 
-os.chdir('..')
+
+os.chdir('../..')
 import versioneer
 
 version = versioneer.get_versions()['version']
