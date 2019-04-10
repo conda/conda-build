9a0b31ef56cd41c933bff2be1d1fca6ba00d59ad
Nehal J Wani <nehaljw.kkd1@gmail.com>
2019-04-10 09:03:32 +0530

    docs.continuum.io/conda/build.html -> docs.conda.io/projects/conda-build

--- tests/test-skeleton/sympy-0.7.5/bld.bat
+++ tests/test-skeleton/sympy-0.7.5/bld.bat
@@ -4,5 +4,5 @@ if errorlevel 1 exit 1
 :: Add more build steps here, if they are necessary.
 
 :: See
-:: http://docs.continuum.io/conda/build.html
+:: https://docs.conda.io/projects/conda-build
 :: for a list of environment variables that are set during the build process.
