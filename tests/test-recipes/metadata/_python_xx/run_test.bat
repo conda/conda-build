5be7c28479a89efbaef1c728bdb2b274c2319244
Ray Donnelly <mingw.android@gmail.com>
2019-04-25 01:43:00 +0100

    Fix _python_xx on Windows

--- tests/test-recipes/metadata/_python_xx/run_test.bat
+++ tests/test-recipes/metadata/_python_xx/run_test.bat
@@ -1,5 +1,5 @@
 @echo on
 conda list -p "%PREFIX%" --canonical
 if errorlevel 1 exit 1
-conda list -p "%PREFIX%" --canonical | grep "conda-build-test-python-xx-1\.0-py34_0"
+conda list -p "%PREFIX%" --canonical | grep "conda-build-test-python-xx-1\.0-py35_0"
 if errorlevel 1 exit 1
