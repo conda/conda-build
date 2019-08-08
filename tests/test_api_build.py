c5f23c0efb77e1077b9c2f128d7e725fce59f9ce
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2019-08-08 12:14:53 +0100

    Update msvc_vers

--- tests/test_api_build.py
+++ tests/test_api_build.py
@@ -288,7 +288,7 @@ platforms = ["64" if sys.maxsize > 2**32 else "32"]
 if sys.platform == "win32":
     platforms = sorted(list(set(["32", ] + platforms)))
     compilers = ["2.7", "3.6"]
-    msvc_vers = ['9.0', '14.0']
+    msvc_vers = ['9.0', '15.0']
 else:
     msvc_vers = []
     compilers = [".".join([str(sys.version_info.major), str(sys.version_info.minor)])]
