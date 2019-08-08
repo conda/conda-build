f32ce5dc1004713d34db301a6398b7bccded71f3
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2019-08-08 11:48:40 +0100

    Fix test_cmake_generator

--- tests/test_api_build.py
+++ tests/test_api_build.py
@@ -287,7 +287,7 @@ def test_checkout_tool_as_dependency(testing_workdir, testing_config, monkeypatc
 platforms = ["64" if sys.maxsize > 2**32 else "32"]
 if sys.platform == "win32":
     platforms = sorted(list(set(["32", ] + platforms)))
-    compilers = ["2.7", "3.5"]
+    compilers = ["2.7", "3.6"]
     msvc_vers = ['9.0', '14.0']
 else:
     msvc_vers = []
