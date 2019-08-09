1925628fc40e2e72bfe356d41256b20c90eea180
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2019-08-09 10:15:51 +0100

    Update tests - replace python 3.5 by 3.6

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -599,7 +599,7 @@ def test_render_with_python_arg_reduces_subspace(capfd):
     out, err = capfd.readouterr()
     assert(len(out.splitlines()) == 2)
 
-    args = [recipe, '--python=3.5', '--output']
+    args = [recipe, '--python=3.6', '--output']
     main_render.execute(args)
     out, err = capfd.readouterr()
     assert(len(out.splitlines()) == 1)
@@ -614,7 +614,7 @@ def test_render_with_python_arg_reduces_subspace(capfd):
 def test_render_with_python_arg_CLI_reduces_subspace(capfd):
     recipe = os.path.join(metadata_dir, "..", "variants", "20_subspace_selection_cli")
     # build the package
-    args = [recipe, '--variants', '{python: [2.7, 3.5]}', '--output']
+    args = [recipe, '--variants', '{python: [2.7, 3.6]}', '--output']
     main_render.execute(args)
     out, err = capfd.readouterr()
     assert(len(out.splitlines()) == 3)
@@ -624,7 +624,7 @@ def test_render_with_python_arg_CLI_reduces_subspace(capfd):
     out, err = capfd.readouterr()
     assert(len(out.splitlines()) == 2)
 
-    args = [recipe, '--variants', '{python: 3.5}', '--output']
+    args = [recipe, '--variants', '{python: 3.6}', '--output']
     main_render.execute(args)
     out, err = capfd.readouterr()
     assert(len(out.splitlines()) == 1)
