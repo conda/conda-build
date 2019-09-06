688b2f87f659f61bd25d0a08fed55ba7b88aaeae
Marcelo Duarte Trevisani <marceloduartetrevisani@gmail.com>
2019-09-06 13:43:12 +0100

    Update script command on conda skeleton pypi to use `{{ PYTHON }} -m pip install . -vv`

--- conda_build/skeletons/pypi.py
+++ conda_build/skeletons/pypi.py
@@ -344,9 +344,9 @@ def skeletonize(packages, output_dir=".", version=None, recursive=False,
             if noarch_python:
                 ordered_recipe['build']['noarch'] = 'python'
 
-            ordered_recipe['build']['script'] = ('{{ PYTHON }} -m pip install . --no-deps '
-                                                 '--ignore-installed -vv ' +
-                                                 ' '.join(setup_options))
+            ordered_recipe['build']['script'] = (
+                    '{{ PYTHON }} -m pip install . -vv' + ' '.join(setup_options)
+                )
 
             # Always require python as a dependency.  Pip is because we use pip for
             #    the install line.
