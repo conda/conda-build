bf5464553939d5ebdca2e69787ec81fd474b08d3
Ray Donnelly <mingw.android@gmail.com>
2020-03-02 15:14:38 +0100

    Tidy up prefix replacement text

--- conda_build/build.py
+++ conda_build/build.py
@@ -893,10 +893,11 @@ def get_files_with_prefix(m, files_in, prefix):
     all_matches = {}
 
     variant = m.config.variant
+    replacement_tags = ''
     if 'replacements' in variant:
         replacements = variant['replacements']
-        for replacement in replacements['all_replacements']:
-            import glob2
+        last = len(replacements['all_replacements']) - 1
+        for index, replacement in enumerate(replacements['all_replacements']):
             all_matches = have_regex_files(files=[f for f in files if any(
                                                   glob2.fnmatch.fnmatch(f, r) for r in replacement['glob_patterns'])],
                                            prefix=prefix,
@@ -906,11 +907,14 @@ def get_files_with_prefix(m, files_in, prefix):
                                            match_records=all_matches,
                                            regex_rg=replacement['regex_rg'] if 'regex_rg' in replacement else None,
                                            debug=m.config.debug)
+            replacement_tags = replacement_tags + '"' + replacement['tag'] + ('"' if
+                                                         index == last else '", ')
     perform_replacements(all_matches, prefix)
     end = time.time()
-    total_replacements = sum(map(lambda i: len(i['submatches']), all_matches))
-    print("INFO :: Time taken to mark (prefix) and mark+peform (pkg-config, CMake, qmake)\n"
-          "        {} replacements in {} files was {} seconds".format(
+    total_replacements = sum(map(lambda i: len(all_matches[i]['submatches']), all_matches))
+    print("INFO :: Time taken to mark (prefix){}\n"
+          "        {} replacements in {} files was {:.2f} seconds".format(
+          " and mark+peform ({})".format(replacement_tags) if replacement_tags else '',
         total_replacements, len(all_matches), end - start))
     '''
     # Keeping this around just for a while.
