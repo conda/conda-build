d346c158747f83139d68dc73598a43e9502b196c
Ray Donnelly <mingw.android@gmail.com>
2020-04-10 17:56:00 +0200

    Revert "Fix test_skeleton_pypi. Urls and filenames must be lowercase now"
    This reverts commit a57a6d2583a6b58b9d52e2cf51b9670ee033c17b.
    
    We should instead use the canonical project name from PyPI.
    
    FWIW this change fixes Click and breaks Flask

--- conda_build/skeletons/pypi.py
+++ conda_build/skeletons/pypi.py
@@ -64,7 +64,6 @@ ABOUT_ORDER = ['home', 'license', 'license_family', 'license_file', 'summary',
                'doc_url', 'dev_url']
 
 PYPI_META_HEADER = """{{% set name = "{packagename}" %}}
-{{% set namel = name.lower() %}}
 {{% set version = "{version}" %}}
 
 """
@@ -75,11 +74,11 @@ PYPI_META_HEADER = """{{% set name = "{packagename}" %}}
 # etc. is determined by EXPECTED_SECTION_ORDER.
 PYPI_META_STATIC = {
     'package': OrderedDict([
-        ('name', '{{ namel }}'),
+        ('name', '{{ name|lower }}'),
         ('version', '{{ version }}'),
     ]),
     'source': OrderedDict([
-        ('url', '/packages/source/{{ namel[0] }}/{{ namel }}/{{ namel }}-{{ version }}.tar.gz'),  # NOQA
+        ('url', '/packages/source/{{ name[0] }}/{{ name }}/{{ name }}-{{ version }}.tar.gz'),  # NOQA
     ]),
     'build': OrderedDict([
         ('number', 0),
