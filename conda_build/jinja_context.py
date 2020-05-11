47067ed583564f7cc224c6b101bfe41b6d027431
Pradipta Ghosh <pradghos@in.ibm.com>
2020-05-11 02:51:33 -0700

    Enable support for s390x in conda-build

--- conda_build/jinja_context.py
+++ conda_build/jinja_context.py
@@ -427,7 +427,7 @@ def cdt(package_name, config, permit_undefined_jinja=False):
 
     cdt_name = 'cos6'
     arch = config.host_arch or config.arch
-    if arch == 'ppc64le' or arch == 'aarch64' or arch == 'ppc64':
+    if arch == 'ppc64le' or arch == 'aarch64' or arch == 'ppc64' or arch == 's390x':
         cdt_name = 'cos7'
         cdt_arch = arch
     else:
