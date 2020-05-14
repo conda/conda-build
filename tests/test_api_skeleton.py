c06b6b5da72dacbd432d10ee09562c0b66abbd77
Ray Donnelly <mingw.android@gmail.com>
2020-05-14 20:28:17 +0200

    Skip test involving sympy on python 2.7 (let the rot commence)

--- tests/test_api_skeleton.py
+++ tests/test_api_skeleton.py
@@ -412,6 +412,8 @@ def test_setuptools_test_requirements(testing_workdir):
     assert m.meta['test']['requires'] == ['nose >=1.0']
 
 
+@pytest.mark.skipif(sys.version[:3] == "2.7",
+                    reason="sympy is python 3.5 and above")
 def test_pypi_section_order_preserved(testing_workdir):
     """
     Test whether sections have been written in the correct order.
