412d36c2d4d66283cb835e5d8ce9f1f95093f7a5
Ray Donnelly <mingw.android@gmail.com>
2020-05-21 10:33:04 +0200

    Start to support noarch: python builds on Windows

--- conda_build/build.py
+++ conda_build/build.py
@@ -1383,6 +1383,16 @@ def post_process_files(m, initial_prefix_files):
     # The post processing may have deleted some files (like easy-install.pth)
     current_prefix_files = utils.prefix_files(prefix=m.config.host_prefix)
     new_files = sorted(current_prefix_files - initial_prefix_files)
+    '''
+    if m.noarch == 'python' and m.config.subdir == 'win-32':
+        # Delete any PIP-created .exe launchers and fix entry_points.txt
+        # .. but we need to provide scripts instead here.
+        from conda_build.post import caseless_sepless_fnmatch
+        exes = caseless_sepless_fnmatch(new_files, 'Scripts/*.exe')
+        for ff in exes:
+            os.unlink(os.path.join(m.config.host_prefix, ff))
+            new_files.remove(ff)
+    '''
     new_files = utils.filter_files(new_files, prefix=m.config.host_prefix)
 
     host_prefix = m.config.host_prefix
