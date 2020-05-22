2f2003378db120f942e15cb6e33bae8ba2364e31
Ray Donnelly <mingw.android@gmail.com>
2020-05-22 15:49:26 +0200

    Fix has_prefix_files on Windows

--- tests/test-recipes/metadata/has_prefix_files/bld.bat
+++ tests/test-recipes/metadata/has_prefix_files/bld.bat
@@ -1,5 +1,6 @@
 @echo on
 echo %PREFIX% > "%PREFIX%\automatic-prefix"
+echo %PREFIX% > "%PREFIX%\unlisted-text-prefix"
 echo /opt/anaconda1anaconda2anaconda3 > "%PREFIX%\has-prefix"
 python "%RECIPE_DIR%\write_binary_has_prefix.py"
 python "%RECIPE_DIR%\write_forward_slash_prefix.py"
