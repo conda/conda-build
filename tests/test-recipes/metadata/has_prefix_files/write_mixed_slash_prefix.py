714afc3df6441de3b1285302c48b3fdcdb1180b4
Ray Donnelly <mingw.android@gmail.com>
2020-06-14 00:55:26 +0200

    has_prefix_files: Fix emission of mixed-slash-prefix => next this gets split into mixed-slash-prefixes and mixed-slash-prefix

--- tests/test-recipes/metadata/has_prefix_files/write_mixed_slash_prefix.py
+++ tests/test-recipes/metadata/has_prefix_files/write_mixed_slash_prefix.py
@@ -1,13 +1,13 @@
 import os
 import re, itertools
 from os.path import join
-
 prefix = os.environ['PREFIX']
 fn = os.path.join(prefix, 'mixed-slash-prefix')
 the_sep = '\\' if '\\' in prefix else '/'
 other_sep = '\\' if the_sep == '/' else '\\'
-
 with open(fn, 'w') as f:
     f.write(join(prefix.replace(the_sep, '/'))+'\n')
     f.write(join(prefix.replace(the_sep, '\\'))+'\n')
-    f.write(re.sub('(/)', lambda m, c=itertools.count(): m.group() if next(c) % 2 else other_sep, prefix))
+    bits_prefix = prefix.replace(the_sep, '/').split('/')
+    bits_slashed = ['\\' + folder if index % 2 else '/' + folder for index, folder in enumerate(bits_prefix)]
+    f.write(''.join(bits_slashed))
