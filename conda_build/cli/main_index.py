50af2a328de42eb8613f4a1a4548e454d44d8858
Connor Martin <connormartin7@gmail.com>
2020-10-01 13:48:45 -0500

    index by file

--- conda_build/cli/main_index.py
+++ conda_build/cli/main_index.py
@@ -75,6 +75,11 @@ def parse_args(args):
         will keep python 2.7.X and 3.6.Y in the current_index.json, instead of only the very latest python version.
         """
     )
+    p.add_argument(
+        "-f", "--file",
+        help="A file that contains a new line separated list of packages to add to repodata.",
+        action="store"
+    )
 
     args = p.parse_args(args)
     return p, args
@@ -86,7 +91,7 @@ def execute(args):
     api.update_index(args.dir, check_md5=args.check_md5, channel_name=args.channel_name,
                      threads=args.threads, subdir=args.subdir, patch_generator=args.patch_generator,
                      verbose=args.verbose, progress=args.progress, hotfix_source_repo=args.hotfix_source_repo,
-                     current_index_versions=args.current_index_versions_file)
+                     current_index_versions=args.current_index_versions_file, index_file=args.file)
 
 
 def main():
