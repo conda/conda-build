3ae7b8a20de7d5fd745d2d8b6cc9ba464ec0bd9a
Ray Donnelly <mingw.android@gmail.com>
2020-10-30 05:50:18 +0100

    Add Setting and build/noarch_python_build_age and fix tests not finding packages
    I am addressing two different concerns here but they're in the same bits of code.

--- conda_build/config.py
+++ conda_build/config.py
@@ -123,6 +123,8 @@ def _get_default_settings():
             #    cli/main_build.py that this default will switch in conda-build 4.0.
             Setting('error_overlinking', cc_conda_build.get('error_overlinking',
                                                            'false').lower() == 'true'),
+            Setting('noarch_python_build_age', cc_conda_build.get('noarch_python_build_age',
+                                                           0)),
             Setting('enable_static', cc_conda_build.get('enable_static',
                                                            'false').lower() == 'true'),
             Setting('error_overdepending', cc_conda_build.get('error_overdepending',
