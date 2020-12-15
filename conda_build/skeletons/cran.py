92cea0245725926a1568b5efc983d876c6d4a313
Isuru Fernando <isuru.11@cse.mrt.ac.lk>
2020-12-14 21:43:18 -0600

    Fix R_ARGS

--- conda_build/skeletons/cran.py
+++ conda_build/skeletons/cran.py
@@ -133,7 +133,7 @@ export DISABLE_AUTOBREW=1
 mv DESCRIPTION DESCRIPTION.old
 grep -va '^Priority: ' DESCRIPTION.old > DESCRIPTION
 # shellcheck disable=SC2086
-${{R}} CMD INSTALL --build . ${R_ARGS}
+${{R}} CMD INSTALL --build . ${{R_ARGS}}
 
 # Add more build steps here, if they are necessary.
 
