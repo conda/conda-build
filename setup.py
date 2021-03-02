3ea3da78a7f4f0e65541bb164d50436f1a15d840
Marcelo Trevisani <marceloduartetrevisani@gmail.com>
2021-03-02 06:27:19 +0000

    Drop Python 2 classifiers

--- setup.py
+++ setup.py
@@ -40,8 +40,6 @@ setup(
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
