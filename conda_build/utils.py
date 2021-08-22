63bd1dbb88fd00963af92088c7d53afa28961d97
Isuru Fernando <isuruf@gmail.com>
2021-08-22 17:23:33 -0500

    Fix for when version has stars

--- conda_build/utils.py
+++ conda_build/utils.py
@@ -1514,10 +1514,16 @@ def apply_pin_expressions(version, min_pin='x.x.x.x.x.x.x', max_pin='x'):
             if versions[p_idx][-1] == '.':
                 versions[p_idx] = versions[p_idx][:-1]
     if versions[0]:
-        if VersionOrder(version) < VersionOrder(versions[0]):
+        if version.endswith(".*"):
+            version_order = VersionOrder(version[:-2])
+        elif version.endswith("*"):
+            version_order = VersionOrder(version[:-1])
+        else:
+            version_order = VersionOrder(version)
+        if version_order < VersionOrder(versions[0]):
             # If the minimum is greater than the version this is a pre-release build.
             # Use the version as the lower bound
-            versions[0] = '>=' + str(version)
+            versions[0] = '>=' + version
         else:
             versions[0] = '>=' + versions[0]
     if versions[1]:
