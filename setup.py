764eed19ae3b97080047012efc3553d44aa6c9e7
Jannis Leidel <jannis@leidel.info>
2021-12-01 11:38:56 +0100

    Add missing dependencies to setup.py.
    glob2 and conda-package-handling were missing, although conda-package-handling currently is actually missing a PyPI release (commented out because of that).

--- setup.py
+++ setup.py
@@ -5,12 +5,27 @@ from setuptools import setup
 
 # Don't proceed with 'unknown' in version
 version_dict = versioneer.get_versions()
-if version_dict['error']:
+if version_dict["error"]:
     raise RuntimeError(version_dict["error"])
 
-deps = ['conda', 'requests', 'filelock', 'pyyaml', 'jinja2', 'pkginfo',
-        'beautifulsoup4', 'chardet', 'pytz', 'tqdm', 'psutil', 'six',
-        'libarchive-c', 'setuptools']
+deps = [
+    "conda",
+    "requests",
+    "filelock",
+    "pyyaml",
+    "jinja2",
+    "pkginfo",
+    "beautifulsoup4",
+    "chardet",
+    "pytz",
+    "tqdm",
+    "psutil",
+    "six",
+    "libarchive-c",
+    "setuptools",
+    # "conda-package-handling",  # remove comment once released on PyPI
+    "glob2",
+]
 
 # We cannot build lief for Python 2.7 on Windows (unless we use mingw-w64 for it, which
 # would be a non-trivial amount of work).
@@ -21,7 +36,7 @@ deps = ['conda', 'requests', 'filelock', 'pyyaml', 'jinja2', 'pkginfo',
 
 setup(
     name="conda-build",
-    version=version_dict['version'],
+    version=version_dict["version"],
     cmdclass=versioneer.get_cmdclass(),
     author="Continuum Analytics, Inc.",
     author_email="conda@continuum.io",
@@ -37,23 +52,29 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
     ],
-    python_requires='>=3',
+    python_requires=">=3.6",
     description="tools for building conda packages",
-    long_description=open('README.rst').read(),
-    packages=['conda_build', 'conda_build.cli',
-              'conda_build.skeletons', 'conda_build.os_utils'],
+    long_description=open("README.rst").read(),
+    packages=[
+        "conda_build",
+        "conda_build.cli",
+        "conda_build.skeletons",
+        "conda_build.os_utils",
+    ],
     entry_points={
-        'console_scripts': ['conda-build = conda_build.cli.main_build:main',
-                            'conda-convert = conda_build.cli.main_convert:main',
-                            'conda-develop = conda_build.cli.main_develop:main',
-                            'conda-index = conda_build.cli.main_index:main',
-                            'conda-inspect = conda_build.cli.main_inspect:main',
-                            'conda-metapackage = conda_build.cli.main_metapackage:main',
-                            'conda-render = conda_build.cli.main_render:main',
-                            'conda-skeleton = conda_build.cli.main_skeleton:main',
-                            'conda-debug = conda_build.cli.main_debug:main',
-                            ]},
+        "console_scripts": [
+            "conda-build = conda_build.cli.main_build:main",
+            "conda-convert = conda_build.cli.main_convert:main",
+            "conda-develop = conda_build.cli.main_develop:main",
+            "conda-index = conda_build.cli.main_index:main",
+            "conda-inspect = conda_build.cli.main_inspect:main",
+            "conda-metapackage = conda_build.cli.main_metapackage:main",
+            "conda-render = conda_build.cli.main_render:main",
+            "conda-skeleton = conda_build.cli.main_skeleton:main",
+            "conda-debug = conda_build.cli.main_debug:main",
+        ]
+    },
     install_requires=deps,
-    package_data={'conda_build': ['templates/*', 'cli-*.exe']},
+    package_data={"conda_build": ["templates/*", "cli-*.exe"]},
     zip_safe=False,
 )
