cd27ee24b3cf2d14182a0660dcfbf16ac4263e65
Jannis Leidel <jannis@leidel.info>
2021-12-07 11:19:05 +0100

    Build absolute path for some metadata tests.

--- tests/test-recipes/metadata/ignore_some_prefix_files/run_test.py
+++ tests/test-recipes/metadata/ignore_some_prefix_files/run_test.py
@@ -1,7 +1,7 @@
 from glob import glob
 import os
 
-pkgs = os.path.join(os.environ["ROOT"], "pkgs")
+pkgs = os.path.abspath(os.path.join(os.environ["ROOT"], "pkgs"))
 pkg_dir = glob(os.path.join(pkgs, "conda-build-test-ignore-some-prefix-files-1.0-0"))[0]
 info_dir = os.path.join(pkg_dir, 'info')
 has_prefix_file = os.path.join(info_dir, "has_prefix")
