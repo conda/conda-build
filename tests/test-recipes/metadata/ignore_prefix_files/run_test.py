4702f527c8dde3d843c3d84e08ba716b0bb804dd
Rylan <rchord@users.noreply.github.com>
2021-12-10 10:42:09 -0700

    Adding a debug comment to see the path

--- tests/test-recipes/metadata/ignore_prefix_files/run_test.py
+++ tests/test-recipes/metadata/ignore_prefix_files/run_test.py
@@ -6,6 +6,9 @@ print(os.environ["ROOT"])
 print("^^^^^ENVIRON^^^^^^")
 print(os.environ)
 pkgs = os.path.abspath(os.path.join(os.environ["ROOT"], "pkgs"))
+for path in glob(os.path.join(pkgs, "conda-build-test-ignore-prefix-files-1.0-0")):
+    print("--------")
+    print(path)
 pkg_dir = glob(os.path.join(pkgs, "conda-build-test-ignore-prefix-files-1.0-0"))[0]
 info_dir = os.path.join(pkg_dir, 'info')
 assert os.path.isdir(info_dir)
