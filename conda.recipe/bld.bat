58e9c9987ca95da69dc2e07f9573876fcef3a536
Daniel Bast <2790401+dbast@users.noreply.github.com>
2022-01-26 16:11:58 +0100

    Support bdist_conda with setuptools >=60.0.0
    The distutils included in setuptools >=60.0.0 are enabled by default, when
    the setuptools package is imported, see
    https://setuptools.pypa.io/en/latest/history.html#v60-0-0

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -1,7 +1,14 @@
 python setup.py install --single-version-externally-managed --record=record.txt
-if errorlevel 1 exit 1
+IF %ERRORLEVEL% NEQ 0 exit 1
 
 del %SCRIPTS%\conda-init
 
 copy bdist_conda.py %PREFIX%\Lib\distutils\command\
-if errorlevel 1 exit 1
+IF %ERRORLEVEL% NEQ 0 exit 1
+
+:: required for distutils included in setuptools >=60.0.0
+:: see also https://setuptools.pypa.io/en/latest/history.html#v60-0-0
+set "SETUPTOOLS_DISTUTILS="%PREFIX%\Lib\site-packages\setuptools\_distutils\command\"
+mkdir "%SETUPTOOLS_DISTUTILS%"
+copy bdist_conda.py "%SETUPTOOLS_DISTUTILS%"
+IF %ERRORLEVEL% NEQ 0 exit 1
---
+++ news/bdist_conda_setuptools.rst
@@ -0,0 +1,25 @@
+Enhancements:
+-------------
+
+* <news item>
+
+Bug fixes:
+----------
+
+* Support bdist_conda with setuptools >=60.0.0. The distutils included in setuptools >=60.0.0
+  are now enabled by default, when the setuptools package is imported, see https://setuptools.pypa.io/en/latest/history.html#v60-0-0
+
+Deprecations:
+-------------
+
+* <news item>
+
+Docs:
+-----
+
+* <news item>
+
+Other:
+------
+
+* <news item>
