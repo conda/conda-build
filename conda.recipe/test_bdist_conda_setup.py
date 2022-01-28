8be54d5c38108aba1d66d9f285fc3f7e1bb52a68
Daniel Bast <2790401+dbast@users.noreply.github.com>
2022-01-28 14:56:13 +0100

    Update conda.recipe/test_bdist_conda_setup.py
    Co-authored-by: Jannis Leidel <jannis@leidel.info>

--- conda.recipe/test_bdist_conda_setup.py
+++ conda.recipe/test_bdist_conda_setup.py
@@ -4,5 +4,5 @@ import conda_build.bdist_conda
 setup(
     name="package",
     version="1.0.0",
-    distclass=conda_build.bdist_conda.CondaDistribution
+    distclass=conda_build.bdist_conda.CondaDistribution,
 )
