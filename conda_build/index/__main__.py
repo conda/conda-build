dbb535fb59c14810c6f89758cba2ac94153ed809
Daniel Holth <dholth@anaconda.com>
2022-05-10 17:45:54 -0400

    debug logging

--- conda_build/index/__main__.py
+++ conda_build/index/__main__.py
@@ -1,3 +1,5 @@
 # normally run with "conda index" or bin/conda-index
 import conda_build.cli.main_index
+import logging
+logging.getLogger("conda_build.index").setLevel(logging.DEBUG)
 conda_build.cli.main_index.main()
