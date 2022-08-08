f8b185fe12ee3b258a986e1f0766f160d23477d0
John Kirkham <jakirkham@gmail.com>
2022-08-08 15:32:02 -0700

    Use `toolz` (and drop vendoring)

--- conda_build/inspect_pkg.py
+++ conda_build/inspect_pkg.py
@@ -17,12 +17,14 @@ import tempfile
 from conda_build.os_utils.ldd import get_linkages, get_package_obj_files, get_untracked_obj_files
 from conda_build.os_utils.liefldd import codefile_type
 from conda_build.os_utils.macho import get_rpaths, human_filetype
-from conda_build.utils import (groupby, getter, comma_join, rm_rf, package_has_file, get_logger,
+from conda_build.utils import (comma_join, rm_rf, package_has_file, get_logger,
                                ensure_list)
 
 from conda_build.conda_interface import (iteritems, specs_from_args, is_linked, linked_data, get_index)
 from conda_build.conda_interface import display_actions, install_actions
 from conda_build.conda_interface import memoized
+
+from tlz.itertoolz import getter, groupby
 
 
 @memoized
