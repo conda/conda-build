1c10f43d8605a7a980b4272ae2e4e4ac8170d54c
Ken Odegard <kodegard@anaconda.com>
2022-11-01 15:40:26 -0500

    Drop MSVC++ 9.0 test

--- tests/test_api_build.py
+++ tests/test_api_build.py
@@ -370,9 +370,13 @@ def test_checkout_tool_as_dependency(testing_workdir, testing_config, monkeypatc
 
 platforms = ["64" if sys.maxsize > 2**32 else "32"]
 if sys.platform == "win32":
-    platforms = sorted(list(set(["32", ] + platforms)))
-    compilers = ["3.6", "3.7", pytest.param("2.7", marks=pytest.mark.skip("Failing for Python 2.7"))]
-    msvc_vers = ['9.0', '14.0']
+    platforms = sorted({"32", *platforms})
+    compilers = [
+        "3.6",
+        "3.7",
+        pytest.param("2.7", marks=pytest.mark.skip("Failing for Python 2.7")),
+    ]
+    msvc_vers = ["14.0"]
 else:
     msvc_vers = []
     compilers = [".".join([str(sys.version_info.major), str(sys.version_info.minor)])]
