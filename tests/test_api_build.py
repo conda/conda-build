a819941c06f69418bc707b64f3f7e417e25557ec
Ken Odegard <kodegard@anaconda.com>
2022-12-22 11:55:00 -0600

    Update tests/test_api_build.py

--- tests/test_api_build.py
+++ tests/test_api_build.py
@@ -1180,11 +1180,7 @@ def test_failed_recipe_leaves_folders(testing_config, testing_workdir):
 
     # make sure that it does not leave lock files, though, as these cause permission errors on
     #    centralized installations
-    locks = {lock.lock_file for lock in locks if os.path.isfile(lock.lock_file)}
-    assert not locks, "\n".join(
-        "remaining locks:",
-        *(f"  {lock}" for lock in locks.items()),
-    )
+    assert not {lock.lock_file for lock in locks if os.path.isfile(lock.lock_file)}
 
 
 @pytest.mark.sanity
