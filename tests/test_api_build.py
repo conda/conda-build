394aa199abbf40b3a5312c8de48ee8aae66d117e
sven6002 <svenkatesh@anaconda.com>
2023-01-11 09:06:53 -0600

    Adding flaky test to overlinking test cases

--- tests/test_api_build.py
+++ tests/test_api_build.py
@@ -1396,7 +1396,7 @@ def test_provides_features_metadata(testing_config):
     assert 'provides_features' in index
     assert index['provides_features'] == {'test2': 'also_ok'}
 
-
+@pytest.mark.flaky(reruns=5, reruns_delay=2)
 def test_overlinking_detection(testing_config, variants_conda_build_sysroot):
     testing_config.activate = True
     testing_config.error_overlinking = True
