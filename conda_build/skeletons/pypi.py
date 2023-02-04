fe3647edc7eeaff5c2d4a52df76567ade46d0374
Daniel Holth <dholth@anaconda.com>
2023-02-04 10:46:44 -0500

    adjust distutils patch deps

--- conda_build/skeletons/pypi.py
+++ conda_build/skeletons/pypi.py
@@ -1219,14 +1219,11 @@ def run_setuppy(src_dir, temp_dir, python_version, extra_specs, config, setup_op
     :param temp_dir: Temporary directory for doing for storing pkginfo.yaml
     :type temp_dir: str
     '''
-    # TODO: we could make everyone's lives easier if we include packaging here, because setuptools
-    #    needs it in recent versions.  At time of writing, it is not a package in defaults, so this
-    #    actually breaks conda-build right now.  Omit it until packaging is on defaults.
-    # specs = ['python %s*' % python_version, 'pyyaml', 'setuptools', 'six', 'packaging', 'appdirs']
     subdir = config.host_subdir
     specs = [f'python {python_version}*',
-             'pip', 'pyyaml', 'setuptools'] + (['m2-patch', 'm2-gcc-libs'] if config.host_subdir.startswith('win')
-                                                    else ['patch'])
+             'pip', 'ruamel.yaml', 'setuptools', 'packaging'
+            ] + (['m2-patch', 'm2-gcc-libs'] if config.host_subdir.startswith('win')
+                                             else ['patch'])
     with open(os.path.join(src_dir, "setup.py")) as setup:
         text = setup.read()
         if 'import numpy' in text or 'from numpy' in text:
