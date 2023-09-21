5d461b9aad72e9ddf9790b86820882eaf7384cc7
Daniel Bast <2790401+dbast@users.noreply.github.com>
2023-09-21 16:08:52 +0200

    Fix sphinx errors and warnings
    error:
    * `conda/conda-build/CHANGELOG.md:2777: WARNING: Substitution error:UndefinedError: 'compiler' is undefined [myst.substitution`
    
    warnings:
    * `WARNING: The pre-Sphinx 1.0 'intersphinx_mapping' format is deprecated and will be removed in Sphinx 8. Update to the current format as described in the documentation. Hint: "intersphinx_mapping = {'<name>': ('https://docs.python.org/', None)}".https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html#confval-intersphinx_mapping`
    * `WARNING: Invalid configuration value found: 'language = None'. Update your configuration to a valid language code. Falling back to 'en' (English).`

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -88,7 +88,7 @@ master_doc = "index"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -206,7 +206,7 @@ texinfo_documents = [
 # -- Options for intersphinx extension ---------------------------------------
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {"https://docs.python.org/": None}
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
 
 # -- Options for todo extension ----------------------------------------------
 
