42b95535e1027872e6501d8710b4f96ae681557f
Isuru Fernando <isuruf@gmail.com>
2023-10-16 11:19:02 -0500

    make one condition for .so

--- conda_build/environ.py
+++ conda_build/environ.py
@@ -581,9 +581,7 @@ def get_shlib_ext(host_platform):
         return ".dll"
     elif host_platform in ["osx", "darwin"]:
         return ".dylib"
-    elif host_platform.startswith("linux") or host_platform.endswith("-wasm32"):
-        return ".so"
-    elif host_platform.startswith("freebsd"):
+    elif host_platform.startswith(("linux", "freebsd")) or host_platform.endswith("-wasm32"):
         return ".so"
     elif host_platform == "noarch":
         # noarch packages should not contain shared libraries, use the system
