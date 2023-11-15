98fd4e9167c18bd7a19bf3f0e0c328bb6515736c
Marcel Bargull <marcel.bargull@udo.edu>
2023-11-15 14:15:26 +0100

    Remove unused imports
    Signed-off-by: Marcel Bargull <marcel.bargull@udo.edu>

--- conda_build/_legacy_conda_imports/conda_imports.py
+++ conda_build/_legacy_conda_imports/conda_imports.py
@@ -14,18 +14,15 @@ from conda.exports import (
     normalized_version,
 )
 
-from conda.auxlib.entity import Entity, EntityType, IntegerField, StringField
 from conda.base.constants import (
     CONDA_PACKAGE_EXTENSIONS,
     DEFAULTS_CHANNEL_NAME,
     UNKNOWN_CHANNEL,
 )
 from conda.base.context import stack_context_default
-from conda.common.io import dashlist, env_vars
-from conda.common.iterators import groupby_to_dict
+from conda.common.io import env_vars
 from conda.common.url import is_url
 from conda.core.index import LAST_CHANNEL_URLS, get_index
 from conda.core.link import PrefixSetup, UnlinkLinkTransaction
 from conda.core.prefix_data import PrefixData
 from conda.models.channel import prioritize_channels
-from conda.models.enums import LinkType
