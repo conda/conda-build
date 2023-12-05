7ac4b4679c7033dedcb2d6ded8ea5cf83471bb5e
Travis Hathaway <travis.j.hathaway@gmail.com>
2023-12-05 15:09:53 +0100

    adding count.js to repository

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -157,7 +157,7 @@ html_extra_path = ["robots.txt"]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ["_static"]
+html_static_path = ["_static"]
 
 # Custom CSS rules
 # html_style = "css/custom.css"
