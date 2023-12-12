f955083866d8b18a5f70644f30e702c7c9501b58
Ken Odegard <kodegard@anaconda.com>
2023-12-11 21:53:42 -0600

    Skip using tmp_env in older conda

--- tests/test_inspect_pkg.py
+++ tests/test_inspect_pkg.py
@@ -1,11 +1,22 @@
 # Copyright (C) 2014 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
+from __future__ import annotations
+
+import pytest
+from conda import __version__ as conda_version
 from conda.core.prefix_data import PrefixData
-from conda.testing import TmpEnvFixture
+from packaging.version import Version, parse
 
 from conda_build.inspect_pkg import which_package
 
+if TYPE_CHECKING := False:
+    from conda.testing import TmpEnvFixture
 
+
+@pytest.mark.skipif(
+    parse(conda_version) < Version("23.5.0"),
+    reason="tmp_env fixture first available in conda 23.5.0",
+)
 def test_which_package(tmp_env: TmpEnvFixture):
     with tmp_env("ca-certificates") as prefix:
         pd = PrefixData(prefix)
