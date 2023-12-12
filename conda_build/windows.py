3a8eb9bb68d4413c765538d94125411dc788e056
Finn Womack <flan313@gmail.com>
2023-12-11 19:53:56 -0800

    Update conda_build/windows.py
    Co-authored-by: Isuru Fernando <isuruf@gmail.com>

--- conda_build/windows.py
+++ conda_build/windows.py
@@ -110,8 +110,8 @@ def msvc_env_cmd(bits, config, override=None):
         "If this recipe does not use a compiler, this message is safe to ignore.  "
         "Otherwise, use {{compiler('<language>')}} jinja2 in requirements/build."
     )
-    if bits == "arm64":
-        log.warn("The legacy MSVC compiler setup does not support arm64 builds. ")
+    if bits not in ["64", "32"]:
+        log.warn(f"The legacy MSVC compiler setup does not support {bits} builds. ")
         return ""
     if override:
         log.warn(
