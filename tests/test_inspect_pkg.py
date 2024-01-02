e9b90ea993c2e977f49d574d8c4520a935a64807
Ken Odegard <kodegard@anaconda.com>
2024-01-02 16:25:54 -0600

    Add benchmark for which_package

--- tests/test_inspect_pkg.py
+++ tests/test_inspect_pkg.py
@@ -3,8 +3,11 @@
 from __future__ import annotations
 
 import json
+import os
 from pathlib import Path
+from uuid import uuid4
 
+import pytest
 from conda.core.prefix_data import PrefixData
 
 from conda_build.inspect_pkg import which_package
@@ -117,3 +120,48 @@ def test_which_package(tmp_path: Path):
     precs_hardlinkB = list(which_package(tmp_path / "hardlinkB", tmp_path))
     assert len(precs_hardlinkB) == 2
     assert set(precs_hardlinkB) == {precA, precB}
+
+
+@pytest.mark.benchmark
+def test_which_package_battery(tmp_path: Path):
+    # regression: https://github.com/conda/conda-build/issues/5126
+    # create a dummy environment
+    (tmp_path / "conda-meta").mkdir()
+    (tmp_path / "conda-meta" / "history").touch()
+    (tmp_path / "lib").mkdir()
+
+    # dummy packages with files
+    for _ in range(100):
+        name = f"package_{uuid4().hex}"
+        files = [f"lib/{uuid4().hex}" for _ in range(100)]
+        for file in files:
+            (tmp_path / file).touch()
+        (tmp_path / "conda-meta" / f"{name}-1-0.json").write_text(
+            json.dumps(
+                {
+                    "build": "0",
+                    "build_number": 0,
+                    "channel": f"{name}-channel",
+                    "files": files,
+                    "name": name,
+                    "paths_data": {
+                        "paths": [
+                            {"_path": file, "path_type": "hardlink", "size_in_bytes": 0}
+                            for file in files
+                        ],
+                        "paths_version": 1,
+                    },
+                    "version": "1",
+                }
+            )
+        )
+
+    # every path should return exactly one package
+    for subdir, _, files in os.walk(tmp_path / "lib"):
+        for file in files:
+            path = Path(subdir, file)
+
+            assert len(list(which_package(path, tmp_path))) == 1
+
+    # missing files should return no packages
+    assert not len(list(which_package(tmp_path / "missing", tmp_path)))
