f81c98324b6100365126e2c9241af393dc724d8b
Marcel Bargull <marcel.bargull@udo.edu>
2024-01-19 05:12:10 +0100

    Remove all unused code from dist string handling

--- conda_build/_legacy_conda_imports/conda_imports.py
+++ conda_build/_legacy_conda_imports/conda_imports.py
@@ -5,22 +5,18 @@ except ImportError:  # pragma: no cover
 
 from conda.exports import (
     Channel,
-    CondaError,
     MatchSpec,
     PackageRecord,
     ProgressiveFetchExtract,
     on_win,
-    normalized_version,
 )
 
 from conda.base.constants import (
-    CONDA_PACKAGE_EXTENSIONS,
     DEFAULTS_CHANNEL_NAME,
     UNKNOWN_CHANNEL,
 )
 from conda.base.context import context, stack_context_default
 from conda.common.io import env_vars
-from conda.common.url import is_url
 from conda.core.index import LAST_CHANNEL_URLS, get_index
 from conda.core.link import PrefixSetup, UnlinkLinkTransaction
 from conda.core.prefix_data import PrefixData
--- conda_build/_legacy_conda_imports/dist.py
+++ conda_build/_legacy_conda_imports/dist.py
@@ -2,78 +2,12 @@
 # SPDX-License-Identifier: BSD-3-Clause
 """(Legacy) Low-level implementation of a Channel."""
 import re
-from typing import NamedTuple
 
-from .conda_imports import (
-    CONDA_PACKAGE_EXTENSIONS,
-    UNKNOWN_CHANNEL,
-    CondaError,
-    PackageRecord,
-    is_url,
-)
+from conda.base.constants import CONDA_PACKAGE_EXTENSIONS
 
 
-class DistDetails(NamedTuple):
-    name: str
-    version: str
-    build: str
-    build_number: str
-    dist_name: str
-    fmt: str
-
-
-def _strip_extension(original_dist):
-    for ext in CONDA_PACKAGE_EXTENSIONS:
-        if original_dist.endswith(ext):
-            original_dist = original_dist[: -len(ext)]
-    return original_dist
-
-
-def _split_extension(original_dist):
-    stripped = _strip_extension(original_dist)
-    return stripped, original_dist[len(stripped) :]
-
-
-def _parse_dist_name(string):
-    original_string = string
-    try:
-        no_fmt_string, fmt = _split_extension(string)
-
-        # remove any directory or channel information
-        if "::" in no_fmt_string:
-            dist_name = no_fmt_string.rsplit("::", 1)[-1]
-        else:
-            dist_name = no_fmt_string.rsplit("/", 1)[-1]
-
-        parts = dist_name.rsplit("-", 2)
-
-        name = parts[0]
-        version = parts[1]
-        build = parts[2] if len(parts) >= 3 else ""
-        build_number_as_string = "".join(
-            filter(
-                lambda x: x.isdigit(),
-                (build.rsplit("_")[-1] if build else "0"),
-            )
-        )
-        build_number = int(build_number_as_string) if build_number_as_string else 0
-
-        return DistDetails(
-            name, version, build, build_number, dist_name, fmt
-        )
-
-    except:
-        raise CondaError(
-            "dist_name is not a valid conda package: %s" % original_string
-        )
-
-
-_not_set = object()
-
-
-def _as_dict_from_string(string, channel_override=_not_set):
-    if is_url(string) and channel_override == _not_set:
-        raise NotImplementedError()
+def dist_string_from_package_record(package_record):
+    string = package_record.fn
 
     if string.endswith("@"):
         raise NotImplementedError()
@@ -85,29 +19,8 @@ def _as_dict_from_string(string, channel_override=_not_set):
     )
     channel, original_dist, w_f_d = re.search(REGEX_STR, string).groups()
 
-    original_dist, fmt = _split_extension(original_dist)
-
-    if channel_override != _not_set:
-        channel = channel_override
-    if not channel:
-        channel = UNKNOWN_CHANNEL
-
-    # enforce dist format
-    dist_details = _parse_dist_name(original_dist)
-    return dict(
-        channel=channel,
-        name=dist_details.name,
-        version=dist_details.version,
-        build=dist_details.build,
-        build_number=dist_details.build_number,
-        dist_name=original_dist,
-        fmt=fmt,
-    )
-
-
-def dist_string_from_package_record(package_record):
-    dist_kwargs = _as_dict_from_string(
-        package_record.fn,
-        channel_override=package_record.channel.canonical_name,
-    )
-    return dist_kwargs["dist_name"]
+    stripped = original_dist
+    for ext in CONDA_PACKAGE_EXTENSIONS:
+        if stripped.endswith(ext):
+            stripped = stripped[: -len(ext)]
+    return stripped
