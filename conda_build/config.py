380a41c2df910a0115a438bd2e6a106a63c83ea9
Marcel Bargull <marcel.bargull@udo.edu>
2024-02-27 08:18:05 +0100

    Use pickle's loads/dumps as a faster deepcopy
    Signed-off-by: Marcel Bargull <marcel.bargull@udo.edu>

--- conda_build/config.py
+++ conda_build/config.py
@@ -9,6 +9,7 @@ from __future__ import annotations
 import copy
 import math
 import os
+import pickle
 import re
 import shutil
 import time
@@ -817,9 +818,10 @@ class Config:
 
     def copy(self):
         new = copy.copy(self)
-        new.variant = copy.deepcopy(self.variant)
+        # Use picke.loads(pickle.dumps(...) as a faster copy.deepcopy alternative.
+        new.variant = pickle.loads(pickle.dumps(self.variant, -1))
         if hasattr(self, "variants"):
-            new.variants = copy.deepcopy(self.variants)
+            new.variants = pickle.loads(pickle.dumps(self.variants, -1))
         return new
 
     # context management - automatic cleanup if self.dirty or self.keep_old_work is not True
