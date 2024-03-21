e598d6a4b488860fea45d83283a59a62f478ec6a
Daniel Ching <carterbox@users.noreply.github.com>
2024-03-21 16:43:53 -0500

    TST: Robust testing of new filematching

--- tests/test-recipes/split-packages/copying_files/bld.bat
+++ tests/test-recipes/split-packages/copying_files/bld.bat
@@ -3,3 +3,4 @@ mkdir %PREFIX%\somedir
 echo "weee" > %PREFIX%\somedir\subpackage_file1
 echo "weee" > %PREFIX%\subpackage_file1.ext
 echo "weee" > %PREFIX%\subpackage_file2.ext
+echo "weee" > %PREFIX%\subpackage_file3.ext
--- tests/test-recipes/split-packages/copying_files/subpackage_test.py
+++ tests/test-recipes/split-packages/copying_files/subpackage_test.py
@@ -6,32 +6,52 @@ assert os.path.isfile(filename)
 contents = open(filename).read().rstrip()
 if hasattr(contents, 'decode'):
     contents = contents.decode()
-assert "weee" in contents, 'incorrect file contents: %s' % contents
-print("plain file OK")
+assert 'weee' in contents, 'incorrect file contents: %s' % contents
+print('plain file OK')
 
 filename = os.path.join(os.environ['PREFIX'], 'somedir', 'subpackage_file1')
-assert os.path.isfile(filename), filename + " is missing"
+assert os.path.isfile(filename), filename + ' is missing'
 contents = open(filename).read().rstrip()
 if hasattr(contents, 'decode'):
     contents = contents.decode()
-assert "weee" in contents, 'incorrect file contents: %s' % contents
-print("subfolder file OK")
+assert 'weee' in contents, 'incorrect file contents: %s' % contents
+print('subfolder file OK')
 
 filename = os.path.join(os.environ['PREFIX'], 'subpackage_file1.ext')
 assert os.path.isfile(filename)
 contents = open(filename).read().rstrip()
 if hasattr(contents, 'decode'):
     contents = contents.decode()
-assert "weee" in contents, 'incorrect file contents: %s' % contents
+assert 'weee' in contents, 'incorrect file contents: %s' % contents
 
 filename = os.path.join(os.environ['PREFIX'], 'subpackage_file2.ext')
 assert os.path.isfile(filename)
 contents = open(filename).read().rstrip()
 if hasattr(contents, 'decode'):
     contents = contents.decode()
-assert "weee" in contents, 'incorrect file contents: %s' % contents
-print("glob OK")
+assert 'weee' in contents, 'incorrect file contents: %s' % contents
+print('glob OK')
 
-filename = os.path.join(os.environ['PREFIX'], 'subpackage_file3.ext')
-assert not os.path.isfile(filename)
-print("glob exclude OK")
+external_host_file = 'lib/libdav1d.so.7'
+if 'osx' in os.getenv('target_platform', ''):
+    external_host_file = 'lib/libdav1d.7.dylib'
+if 'win' in os.getenv('target_platform', ''):
+    external_host_file = 'Library/bin/dav1d.dll'
+
+if os.getenv('PKG_NAME') == 'my_script_subpackage_files':
+    filename = os.path.join(os.environ['PREFIX'], 'subpackage_file3.ext')
+    assert os.path.isfile(filename)
+    print('glob files OK')
+
+    filename = os.path.join(os.environ['PREFIX'], external_host_file)
+    assert os.path.isfile(filename)
+    print('glob files prefix OK')
+
+if os.getenv('PKG_NAME') == 'my_script_subpackage_include_exclude':
+    filename = os.path.join(os.environ['PREFIX'], 'subpackage_file3.ext')
+    assert not os.path.isfile(filename)
+    print('glob exclude OK')
+
+    filename = os.path.join(os.environ['PREFIX'], external_host_file)
+    assert not os.path.isfile(filename)
+    print('glob exclude prefix OK')
