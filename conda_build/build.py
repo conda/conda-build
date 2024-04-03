805d41f7bdea6dd899018e8c523cc738cef99975
Ken Odegard <kodegard@anaconda.com>
2024-04-02 20:16:29 -0500

    More `conda_build.conda_interface` deprecations (#5251)

--- conda_build/build.py
+++ conda_build/build.py
@@ -25,19 +25,17 @@ from bs4 import UnicodeDammit
 from conda import __version__ as conda_version
 from conda.base.context import context, reset_context
 from conda.core.prefix_data import PrefixData
+from conda.exceptions import CondaError, NoPackagesFoundError, UnsatisfiableError
 from conda.models.channel import Channel
 
 from . import __version__ as conda_build_version
 from . import environ, noarch_python, source, tarcheck, utils
 from .conda_interface import (
-    CondaError,
     EntityEncoder,
     FileMode,
     MatchSpec,
-    NoPackagesFoundError,
     PathType,
     TemporaryDirectory,
-    UnsatisfiableError,
     env_path_backup_var_exists,
     prefix_placeholder,
     url_path,
