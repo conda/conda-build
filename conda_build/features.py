de4c1eda40d1d8889456611e92fe6e7c160dd1bb
Bianca Henderson <bhenderson@anaconda.com>
2024-07-11 12:49:38 -0400

    Revert changes to  in order to keep the PR small in scope

--- conda_build/features.py
+++ conda_build/features.py
@@ -1,8 +1,7 @@
 # Copyright (C) 2014 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
 import os
-
-from .exceptions import CondaBuildUserError
+import sys
 
 env_vars = [
     "FEATURE_DEBUG",
@@ -16,7 +15,7 @@ feature_list = []
 for key, value in os.environ.items():
     if key in env_vars:
         if value not in ("0", "1"):
-            raise CondaBuildUserError(
+            sys.exit(
                 f"Error: did not expect environment variable '{key}' "
                 f"being set to '{value}' (not '0' or '1')"
             )
