8e45f2d3c57992f88026ace07285c5617fdcbfc2
Ken Odegard <kodegard@anaconda.com>
2024-07-26 10:50:04 -0500

    Deprecate testing_env fixture

--- tests/cli/test_main_develop.py
+++ tests/cli/test_main_develop.py
@@ -1,30 +1,38 @@
 # Copyright (C) 2014 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
-import os
+from __future__ import annotations
+
 import sys
+from pathlib import Path
+from typing import TYPE_CHECKING
 
 from conda.gateways.connection.download import download
 
 from conda_build.cli import main_develop
 from conda_build.utils import get_site_packages, tar_xf
 
+if TYPE_CHECKING:
+    from conda.testing import TmpEnvFixture
 
-def test_develop(testing_env):
-    f = "https://pypi.io/packages/source/c/conda_version_test/conda_version_test-0.1.0-1.tar.gz"
-    download(f, "conda_version_test.tar.gz")
-    tar_xf("conda_version_test.tar.gz", testing_env)
-    extract_folder = "conda_version_test-0.1.0-1"
-    cwd = os.getcwd()
-    args = ["-p", testing_env, extract_folder]
-    main_develop.execute(args)
-    py_ver = ".".join((str(sys.version_info.major), str(sys.version_info.minor)))
-    with open(
-        os.path.join(get_site_packages(testing_env, py_ver), "conda.pth")
-    ) as f_pth:
-        assert cwd in f_pth.read()
-    args = ["--uninstall", "-p", testing_env, extract_folder]
-    main_develop.execute(args)
-    with open(
-        os.path.join(get_site_packages(testing_env, py_ver), "conda.pth")
-    ) as f_pth:
-        assert cwd not in f_pth.read()
+
+def test_develop(tmp_env: TmpEnvFixture) -> None:
+    py_ver = "%d.%d" % sys.version_info[:2]
+    with tmp_env(f"python={py_ver}") as prefix:
+        download(
+            "https://pypi.io/packages/source/c/conda_version_test/conda_version_test-0.1.0-1.tar.gz",
+            "conda_version_test.tar.gz",
+        )
+        tar_xf("conda_version_test.tar.gz", prefix)
+        extract_folder = "conda_version_test-0.1.0-1"
+
+        main_develop.execute([f"--prefix={prefix}", extract_folder])
+        assert (
+            str(Path.cwd())
+            in Path(get_site_packages(prefix, py_ver), "conda.pth").read_text()
+        )
+
+        main_develop.execute(["--uninstall", f"--prefix={prefix}", extract_folder])
+        assert (
+            str(Path.cwd())
+            not in Path(get_site_packages(prefix, py_ver), "conda.pth").read_text()
+        )
