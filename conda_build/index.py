2708d0bbf51312d2142ba3cd53ace99871f12734
Ken Odegard <kodegard@anaconda.com>
2024-09-20 18:34:15 -0500

    Remove duplicate import

--- conda_build/index.py
+++ conda_build/index.py
@@ -9,7 +9,6 @@ from os.path import dirname
 from typing import TYPE_CHECKING
 
 from conda.base.context import context
-from conda.core.index import Index
 from conda.exceptions import CondaHTTPError
 from conda.utils import url_path
 from conda_index.index import update_index as _update_index
