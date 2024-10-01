23d462ba9f8d6dc8ebd148720c2d22e4df55975b
Ken Odegard <kodegard@anaconda.com>
2024-10-01 16:11:41 -0500

    Convert pytest-benchmark to codspeed test

--- tests/test_index.py
+++ tests/test_index.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import pytest
 from conda.base.context import context
 
 from conda_build.index import get_build_index
@@ -12,14 +13,13 @@ if TYPE_CHECKING:
     from conda_build.metadata import MetaData
 
 
-def test_get_build_index(testing_metadata: MetaData, benchmark) -> None:
-    @benchmark
-    def _():
-        get_build_index(
-            subdir=context.subdir,
-            bldpkgs_dir=testing_metadata.config.bldpkgs_dir,
-            output_folder=testing_metadata.config.output_folder,
-            clear_cache=True,
-            omit_defaults=True,
-            channel_urls=["local", "conda-forge", "defaults"],
-        )
+@pytest.mark.benchmark
+def test_get_build_index(testing_metadata: MetaData) -> None:
+    get_build_index(
+        subdir=context.subdir,
+        bldpkgs_dir=testing_metadata.config.bldpkgs_dir,
+        output_folder=testing_metadata.config.output_folder,
+        clear_cache=True,
+        omit_defaults=True,
+        channel_urls=["local", "conda-forge", "defaults"],
+    )
