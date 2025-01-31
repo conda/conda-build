9af26b4f889d540b6ad4e24e9a482da185f66fd4
jaimergp <jaimergp@users.noreply.github.com>
2025-01-31 19:57:59 +0000

    add test

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -708,3 +708,17 @@ python_min:
             )
         else:
             print("did not parse OK!")
+
+
+def test_extract_single_output_text_with_jinja_is_broken():
+    """
+    Given a recipe with three outputs 1, 2, and 3, where 2 is guarded by a falsy Jinja-if,
+    MetaData.extract_single_output_text() returns 2 when asked for 3.
+
+    This is a bug that should be fixed.
+    """
+    metadata = MetaData(os.path.join(metadata_dir, "_jinja_outputs"))
+    output = metadata.extract_single_output_text("output3", getattr(metadata, "type", None))
+    # We of course want to obtain output3, but the buggy behaviour gave us output2.
+    assert "output3" not in output
+    assert "output2" in output
