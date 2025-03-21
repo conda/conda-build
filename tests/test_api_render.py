01f2aaf2e1641cdfc35fee4aad7927bd82dac2a9
jaimergp <jaimergp@users.noreply.github.com>
2025-03-21 16:24:16 +0100

    pre-commit

--- tests/test_api_render.py
+++ tests/test_api_render.py
@@ -122,7 +122,9 @@ def test_pin_compatible_semver(testing_config):
     assert "zlib >=1.2.11,<2.0a0" in metadata.get_value("requirements/run")
 
 
-@pytest.mark.xfail(reason="Added as part of #5603, reverted in #5647. Fix needs to be reworked.")
+@pytest.mark.xfail(
+    reason="Added as part of #5603, reverted in #5647. Fix needs to be reworked."
+)
 def test_transitive_subpackage_dependency(testing_config):
     recipe_dir = os.path.join(metadata_dir, "transitive_subpackage")
     metadata = api.render(recipe_dir, config=testing_config)[1][0]
