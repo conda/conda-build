c2b867f32195451f97dc18ebbce7176ca221e3d8
Matthew R. Becker <beckermr@users.noreply.github.com>
2025-05-01 18:49:09 -0500

    fix: try pip

--- tests/test-recipes/metadata/entry_points/build.sh
+++ tests/test-recipes/metadata/entry_points/build.sh
@@ -1,5 +1,5 @@
 # conda-build/tests/test-recipes/test-package
 # cd $RECIPE_DIR/../../test-package
 
-# pip install .
-python setup.py install --old-and-unmanageable
+pip install .
+# python setup.py install --old-and-unmanageable
