a5f3de02632f8d24d3a03f66df496ba66b5599fb
Ryan Keith <rkeith@anaconda.com>
2025-07-18 12:08:20 -0700

    Adding in VS code Versioning strings.

--- conda_build/windows.py
+++ conda_build/windows.py
@@ -35,6 +35,9 @@ VS_VERSION_STRING = {
     "11.0": "Visual Studio 11 2012",
     "12.0": "Visual Studio 12 2013",
     "14.0": "Visual Studio 14 2015",
+    "15.0": "Visual Studio 15 2017",
+    "16.0": "Visual Studio 16 2019",
+    "17.0": "Visual Studio 17 2022",
 }
 
 
