1f65810ce52aca2d23ee0cba38ad53b96f77a7d1
Matthew R. Becker <beckermr@users.noreply.github.com>
2025-10-15 10:26:51 -0500

    fix: coerce true/false strings to bools in jinja2
    Add option to coerce string 'true'/'false' to boolean.

--- conda_build/environ.py
+++ conda_build/environ.py
@@ -352,6 +352,7 @@ def get_dict(
     skip_build_id=False,
     escape_backslash=False,
     variant=None,
+    true_false_as_bool=False,
 ):
     if not prefix:
         prefix = m.config.host_prefix
@@ -379,7 +380,11 @@ def get_dict(
     variant = variant or m.config.variant
     for k, v in variant.items():
         if not for_env or (k.upper() not in d and k.upper() not in LANGUAGES):
-            d[k] = v
+            # coerce true and false as strings to bools
+            if true_false_as_bool and isinstance(v, str) and v.lower() in ['true', 'false']:
+                d[k] = v.lower() == 'true'
+            else:
+                d[k] = v
     return d
 
 
