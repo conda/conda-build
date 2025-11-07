168a5eaa3990252fe7a8f89e4da8f926301e87d5
Ryan Keith <rkeith@anaconda.com>
2025-11-07 13:10:05 -0800

    simplifying even more.

--- tests/test-recipes/metadata/cdt_linking/build.sh
+++ tests/test-recipes/metadata/cdt_linking/build.sh
@@ -3,8 +3,8 @@
 set -x
 
 # Simple test that CDT packages can be linked
-# Compile a minimal program that links against X11 and OpenGL from CDT packages
-echo -e "#include <GL/gl.h>\n#include <X11/Xlib.h>\nint main() { Display *d = XOpenDisplay(NULL); glBegin(GL_TRIANGLES); glEnd(); if (d) XCloseDisplay(d); return 0; }">gl.c
-${CC} -o ${PREFIX}/bin/links-to-opengl-cdt -x c -I${PREFIX}/include -L${PREFIX}/lib -lGL -lX11 -Wl,-rpath-link,${PREFIX}/lib gl.c
-find ${PREFIX} -name "libGL*"
+# Compile a minimal program that links against X11 from CDT packages
+echo -e "#include <X11/Xlib.h>\nint main() { Display *d = XOpenDisplay(NULL); if (d) XCloseDisplay(d); return 0; }">x11_test.c
+${CC} -o ${PREFIX}/bin/links-to-x11-cdt -x c -I${PREFIX}/include -L${PREFIX}/lib -lX11 -Wl,-rpath-link,${PREFIX}/lib x11_test.c
+find ${PREFIX} -name "libX11*"
 find ${PREFIX} -name "libc.so*"
