5fd743a5b0e35974102e35fab15692e1695b1d64
Ryan Keith <rkeith@anaconda.com>
2025-11-07 12:50:04 -0800

    Trying to simplify cdt test to be more universal.

--- tests/test-recipes/metadata/cdt_linking/build.sh
+++ tests/test-recipes/metadata/cdt_linking/build.sh
@@ -2,7 +2,9 @@
 
 set -x
 
-echo -e "#include <GL/gl.h>\nint main() { glBegin(GL_TRIANGLES); glEnd(); return 0; }">gl.c
-${CC} -o ${PREFIX}/bin/links-to-opengl-cdt -x c $(pkg-config --libs gl) -Wl,-rpath-link,${PREFIX}/lib gl.c
+# Simple test that CDT packages can be linked
+# Compile a minimal program that links against X11 and OpenGL from CDT packages
+echo -e "#include <GL/gl.h>\n#include <X11/Xlib.h>\nint main() { Display *d = XOpenDisplay(NULL); glBegin(GL_TRIANGLES); glEnd(); if (d) XCloseDisplay(d); return 0; }">gl.c
+${CC} -o ${PREFIX}/bin/links-to-opengl-cdt -x c -I${PREFIX}/include -L${PREFIX}/lib -lGL -lX11 -Wl,-rpath-link,${PREFIX}/lib gl.c
 find ${PREFIX} -name "libGL*"
 find ${PREFIX} -name "libc.so*"
