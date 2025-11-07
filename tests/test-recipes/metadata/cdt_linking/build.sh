37f2e9bbc7feab15e2c6cf7805f6512caaace632
Ryan Keith <rkeith@anaconda.com>
2025-11-07 14:25:00 -0800

    Simplifying test again and adding cdt_name.

--- tests/test-recipes/metadata/cdt_linking/build.sh
+++ tests/test-recipes/metadata/cdt_linking/build.sh
@@ -5,6 +5,6 @@ set -x
 # Simple test that CDT packages can be linked
 # Compile a minimal program that links against X11 from CDT packages
 echo -e "#include <X11/Xlib.h>\nint main() { Display *d = XOpenDisplay(NULL); if (d) XCloseDisplay(d); return 0; }">x11_test.c
-${CC} -o ${PREFIX}/bin/links-to-x11-cdt -x c -I${PREFIX}/include -L${PREFIX}/lib -lX11 -Wl,-rpath-link,${PREFIX}/lib x11_test.c
+${CC} -o ${PREFIX}/bin/links-to-x11-cdt -x c -I${PREFIX}/include -L${PREFIX}/lib -lX11 -lxcb -Wl,-rpath-link,${PREFIX}/lib x11_test.c
 find ${PREFIX} -name "libX11*"
 find ${PREFIX} -name "libc.so*"
