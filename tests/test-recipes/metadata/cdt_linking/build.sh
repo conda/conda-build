7bb3a71d80165665b670f0d1ee324a339f6b168d
Ryan Keith <rkeith@anaconda.com>
2025-11-11 10:59:31 -0800

    Updating docs.

--- tests/test-recipes/metadata/cdt_linking/build.sh
+++ tests/test-recipes/metadata/cdt_linking/build.sh
@@ -4,9 +4,8 @@ set -x
 
 # Simple test that CDT packages can be linked
 # Compile a minimal program that links against PAM and libselinux from CDT packages.
-# This works on main channel currently, it does seem that pam-devel is missing
-# on conda-forge.  In the future, we may want to change this to use so the
-# test can be done on conda-forge.
+# This works on main channel currently, 2025-11-11. It may be worth creating another
+# test that uses conda-forge.
 cat > cdt_test.c << 'EOF'
 #include <stddef.h>
 #include <security/pam_appl.h>
