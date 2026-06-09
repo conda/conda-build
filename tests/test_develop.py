8e74f1698fc83501ecdd7b2bb96421e1c2530ac3
Mahe Iram Khan <65779580+ForgottenProgramme@users.noreply.github.com>
2026-06-09 09:36:41 +0200

    Update tests/test_develop.py
    Co-authored-by: Ken Odegard <kodegard@anaconda.com>

--- tests/test_develop.py
+++ tests/test_develop.py
@@ -16,8 +16,6 @@ from conda_build.utils import rm_rf
 
 from .utils import thisdir
 
-deprecated.module("26.9", "27.3")
-
 
 @pytest.fixture(scope="session")
 def site_packages() -> Generator[Path, None, None]:
